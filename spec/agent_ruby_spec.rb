8de4daf83ccd518f3b8ea454bfbac06b32b7bb06
Matt <matthv@gmail.com>
2023-08-29 17:17:54 +0200

    chore: update rspec example

--- spec/agent_ruby_spec.rb
+++ spec/agent_ruby_spec.rb
@@ -2,8 +2,4 @@ RSpec.describe AgentRuby do
   it "has a version number" do
     expect(AgentRuby::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
