bcd6f7135229f358d4c42fb520dadd150581ed59
Matt <matthv@gmail.com>
2023-09-06 15:03:12 +0200

    chore: add package dry-configurable and setup main class

--- packages/forestadmin_rails/forestadmin_rails.gemspec
+++ packages/forestadmin_rails/forestadmin_rails.gemspec
@@ -21,6 +21,7 @@ admin work on any Rails application (Rails >= 6.1)."
     Dir["{app,config,lib}/**/*", "LICENSE", "Rakefile", "README.md"]
   end
 
-  spec.add_dependency "rails", "~> 6.1"
+  spec.add_dependency "dry-configurable", "~> 1.1"
+  spec.add_dependency "rails", ">= 6.1"
   spec.add_dependency "zeitwerk", "~> 2.3"
 end
--- packages/forestadmin_rails/lib/forestadmin_rails.rb
+++ packages/forestadmin_rails/lib/forestadmin_rails.rb
@@ -1,9 +1,23 @@
+require 'dry-configurable'
 require "forestadmin_rails/version"
 require "forestadmin_rails/engine"
 require "zeitwerk"
+
 loader = Zeitwerk::Loader.for_gem
+loader.ignore("#{__dir__}/generators")
 loader.setup
 
 module ForestadminRails
-  # Your code goes here...
+  extend Dry::Configurable
+
+  setting :debug, default: ENV['FOREST_DEBUG'] || true
+  setting :auth_secret
+  setting :env_secret
+  setting :forest_server_url, default: ENV['FOREST_SERVER_URL'] || 'https://api.forestadmin.com'
+  setting :is_production, default: (ENV['FOREST_ENVIRONMENT'] || 'dev') == 'prod'
+  setting :prefix, default: ENV['FOREST_PREFIX'] || 'forest'
+  setting :permission_expiration, default: ENV['FOREST_PERMISSIONS_EXPIRATION_IN_SECONDS'] || 300
+  setting :cache_dir, default: :memory_store
+  setting :schema_path # , default: Rails.root.join('.forestadmin-schema.json')
+  setting :project_dir # , default: Rails.root
 end
