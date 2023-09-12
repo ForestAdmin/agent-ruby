e8e5548498982a667673c540af251ee794ad12c2
Matt <matthv@gmail.com>
2023-09-12 17:53:42 +0200

    chore: set default values for config

--- packages/forest_admin_rails/lib/forest_admin_rails.rb
+++ packages/forest_admin_rails/lib/forest_admin_rails.rb
@@ -12,24 +12,21 @@ loader.setup
 module ForestAdminRails
   extend Dry::Configurable
 
-  setting :debug, default: ENV['FOREST_DEBUG'] || true
+  setting :debug, default: true
   setting :auth_secret
   setting :env_secret
-  setting :forest_server_url, default: ENV['FOREST_SERVER_URL'] || 'https://api.forestadmin.com'
-  setting :is_production, default: (ENV['FOREST_ENVIRONMENT'] || 'dev') == 'prod'
-  setting :prefix, default: ENV['FOREST_PREFIX'] || 'forest'
-  setting :permission_expiration, default: ENV['FOREST_PERMISSIONS_EXPIRATION_IN_SECONDS'] || 300
+  setting :forest_server_url, default: 'https://api.forestadmin.com'
+  setting :is_production, default: Rails.env.production?
+  setting :prefix, default: 'forest'
+  setting :permission_expiration, default: 300
   setting :cache_dir, default: :'tmp/cache/forest_admin'
-  setting :schema_path # , default: app_root.join('.forest_admin-schema.json')
-  setting :project_dir # , default: app_root
-  setting :loggerLevel, default: ENV['FOREST_LOGGER_LEVEL'] || 'info'
+  setting :schema_path, default: File.join(Dir.pwd, '.forestadmin-schema.json')
+  setting :project_dir, default: Dir.pwd
+  setting :loggerLevel, default: 'info'
   setting :logger, default: nil
 
   if defined?(Rails::Railtie)
     # logic for cors middleware,... here // or it might be into Engine
-  end
-
-  def app_root
-    @app_root ||= (defined?(::Rails.root) && !::Rails.root.nil? ? Rails.root.to_s : Dir.pwd).to_s
+    //
   end
 end
