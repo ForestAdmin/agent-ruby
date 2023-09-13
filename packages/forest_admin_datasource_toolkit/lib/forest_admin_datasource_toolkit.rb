78723cb09de9812aadbabf997ea0562e0f64086b
Nicolas Alexandre <nicolasalexandre9@gmail.com>
2023-09-13 14:57:25 +0200

    feat: add skeleton datasource toolkit gem

---
+++ packages/forest_admin_datasource_toolkit/lib/forest_admin_datasource_toolkit.rb
@@ -0,0 +1,6 @@
+require_relative "forest_admin_datasource_toolkit/version"
+
+module ForestAdminDatasourceToolkit
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ packages/forest_admin_datasource_toolkit/lib/forest_admin_datasource_toolkit/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module ForestAdminDatasourceToolkit
+  VERSION = "0.1.0"
+end
