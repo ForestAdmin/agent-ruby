054d4bae6530dc302b01aca975691310123f80de
Nicolas Alexandre <nicolasalexandre9@gmail.com>
2023-09-14 10:37:55 +0200

    feat: add datasource and collection interface

---
+++ packages/forest_admin_datasource_toolkit/lib/components/contracts/datasource_contract.rb
@@ -0,0 +1,27 @@
+module ForestAdminDatasourceToolkit
+  module Components
+    module Contracts
+      class DatasourceContract
+        def collections
+          raise NotImplementedError, "#{self.class} has not implemented method '#{__method__}'"
+        end
+
+        def charts
+          raise NotImplementedError, "#{self.class} has not implemented method '#{__method__}'"
+        end
+
+        def collection(name)
+          raise NotImplementedError, "#{self.class} has not implemented method '#{__method__}'"
+        end
+
+        def add_collection(collection)
+          raise NotImplementedError, "#{self.class} has not implemented method '#{__method__}'"
+        end
+
+        def render_chart(caller, name)
+          raise NotImplementedError, "#{self.class} has not implemented method '#{__method__}'"
+        end
+      end
+    end
+  end
+end
