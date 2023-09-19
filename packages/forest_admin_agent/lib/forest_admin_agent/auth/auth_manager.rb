4c4362ad566af3e49c12d8b4659e42c65eaf79cd
Matt <matthv@gmail.com>
2023-09-19 12:09:16 +0200

    feat(auth): add response

---
+++ packages/forest_admin_agent/lib/forest_admin_agent/auth/auth_manager.rb
@@ -0,0 +1,50 @@
+require 'json'
+
+module ForestAdminAgent
+  module Auth
+    class AuthManager
+      def initialize
+        @oidc = ForestAdminAgent::Auth::OidcClientManager.new
+      end
+
+      def start(rendering_id)
+        client = @oidc.make_forest_provider rendering_id
+        client.authorization_uri({ state: JSON.generate({ renderingId: rendering_id }) })
+      end
+
+      def verify_code_and_generate_token(params)
+        raise Error, ForestAdminAgent::Utils::ErrorMessages::INVALID_STATE_MISSING unless params['state']
+
+        if Rails.env.development? || Rails.env.test?
+          OpenIDConnect.http_config do |options|
+            options.ssl.verify = false
+          end
+        end
+
+        rendering_id = get_rendering_id_from_state(params['state'])
+
+        forest_provider = @oidc.make_forest_provider rendering_id
+        forest_provider.authorization_code = params['code']
+        access_token = forest_provider.access_token! 'none'
+        resource_owner = forest_provider.get_resource_owner access_token
+
+        resource_owner.make_jwt
+      end
+
+      private
+
+      def get_rendering_id_from_state(state)
+        state = JSON.parse(state.tr("'", '"').gsub('=>', ':'))
+        raise Error, ForestAdminAgent::Utils::ErrorMessages::INVALID_STATE_RENDERING_ID unless state.key? 'renderingId'
+
+        begin
+          Integer(state['renderingId'])
+        rescue ArgumentError
+          raise Error, ForestAdminAgent::Utils::ErrorMessages::INVALID_RENDERING_ID
+        end
+
+        state['renderingId'].to_i
+      end
+    end
+  end
+end
---
+++ packages/forest_admin_agent/lib/forest_admin_agent/routes/security/authentication.rb
@@ -0,0 +1,81 @@
+require 'jwt'
+
+module ForestAdminAgent
+  module Routes
+    module Security
+      class Authentication < AbstractRoute
+        include ForestAdminAgent::Builder
+        def setup_routes
+          add_route(
+            'forest_authentication',
+            'POST',
+            '/authentication', ->(args) { handle_authentication(args) }
+          )
+          add_route(
+            'forest_authentication-callback',
+            'GET',
+            '/authentication/callback', ->(args) { handle_authentication_callback(args) }
+          )
+          add_route(
+            'forest_logout',
+            'POST',
+            '/authentication/logout', ->(args) { handle_authentication_logout(args) }
+          )
+
+          self
+        end
+
+        def handle_authentication(args = {})
+          rendering_id = get_and_check_rendering_id args
+
+          {
+            content: {
+              authorizationUrl: auth.start(rendering_id)
+            }
+          }
+        end
+
+        def handle_authentication_callback(args = {})
+          token = auth.verify_code_and_generate_token(args)
+          token_data = JWT.decode(
+            token,
+            Facades::Container.get(:auth_secret),
+            true,
+            { algorithm: 'HS256' }
+          )[0]
+          {
+            content: {
+              token: token,
+              tokenData: token_data
+            }
+          }
+        end
+
+        def handle_authentication_logout(_args = {})
+          {
+            content: nil,
+            status: 204
+          }
+        end
+
+        def auth
+          ForestAdminAgent::Auth::AuthManager.new
+        end
+
+        protected
+
+        def get_and_check_rendering_id(params)
+          raise Error, ForestAdminAgent::Utils::ErrorMessages::MISSING_RENDERING_ID unless params['renderingId']
+
+          begin
+            Integer(params['renderingId'])
+          rescue ArgumentError
+            raise Error, ForestAdminAgent::Utils::ErrorMessages::INVALID_RENDERING_ID
+          end
+
+          params['renderingId']
+        end
+      end
+    end
+  end
+end
