1a99463e98b40cf8777fb100535308cea73c72f4
Matt <matthv@gmail.com>
2023-09-21 11:39:42 +0200

    feat(auth): add test on healthcheck

---
+++ packages/forest_admin_agent/spec/lib/forest_admin_agent/routes/system/healthcheck_spec.rb
@@ -0,0 +1,36 @@
+require 'spec_helper'
+require 'singleton'
+
+module ForestAdminAgent
+  module Routes
+    module System
+      describe HealthCheck do
+        before do
+          agent_factory = ForestAdminAgent::Builder::AgentFactory.instance
+          agent_factory.setup(
+            {
+              auth_secret: 'cba803d01a4d43b55010cab41fa1ea1f1f51a95e',
+              env_secret: '89719c6d8e2e2de2694c2f220fe2dbf02d5289487364daf1e4c6b13733ed0cdb',
+              is_production: false,
+              cache_dir: 'tmp/cache/forest_admin'
+            }
+          )
+        end
+
+        context 'when testing the HealthCheck class' do
+          it 'returns an empty content and a 204 status' do
+            result = described_class.handle_request
+            expect(result[:content]).to be_nil
+            expect(result[:status]).to eq 204
+          end
+
+          it 'adds the route forest' do
+            described_class.setup_routes
+            expect(described_class.routes.include?('forest')).to be true
+            expect(described_class.routes.length).to eq 1
+          end
+        end
+      end
+    end
+  end
+end
