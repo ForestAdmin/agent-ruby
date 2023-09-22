51ec4bda636d3e41264d8c878816b2f8cfc83809
Matt <matthv@gmail.com>
2023-09-22 17:43:29 +0200

    chore: autoloader set inflection for oauth2 folder

--- packages/forest_admin_agent/lib/forest_admin_agent.rb
+++ packages/forest_admin_agent/lib/forest_admin_agent.rb
@@ -2,6 +2,7 @@ require_relative 'forest_admin_agent/version'
 require 'zeitwerk'
 
 loader = Zeitwerk::Loader.for_gem
+loader.inflector.inflect('oauth2' => 'OAuth2')
 loader.setup
 
 module ForestAdminAgent
