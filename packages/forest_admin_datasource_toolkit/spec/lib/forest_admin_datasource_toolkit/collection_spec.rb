c817f140a7fe3b5618e879a77ca3908cfaa21438
Nicolas Alexandre <nicolasalexandre9@gmail.com>
2023-09-22 16:21:45 +0200

    test: add tests on base collection and base datasource

---
+++ packages/forest_admin_datasource_toolkit/spec/lib/forest_admin_datasource_toolkit/collection_spec.rb
@@ -0,0 +1,48 @@
+require 'spec_helper'
+
+module ForestAdminDatasourceToolkit
+  describe Collection do
+    before do
+      @field = Schema::ColumnSchema.new(column_type: 'String')
+      @datasource = Datasource.new
+      @collection = described_class.new(@datasource, '__collection__')
+    end
+
+    describe 'add_field' do
+      it 'raise when a field with the same name already exist' do
+        expect do
+          @collection.add_field('__duplicated__', @field)
+          @collection.add_field('__duplicated__', @field)
+        end.to raise_error(
+          ForestAdminDatasourceToolkit::Exceptions::ForestException,
+          '🌳🌳🌳 Field __duplicated__ already defined in collection'
+        )
+      end
+
+      it 'add field with unique name' do
+        @collection.add_field('__field__', @field)
+
+        expect(@collection.fields).to eq({ '__field__' => @field })
+      end
+    end
+
+    describe 'add_fields' do
+      it 'add all fields with unique name' do
+        @collection.add_fields(
+          {
+            __first__: @field,
+            __second__: @field,
+          }
+        )
+
+        expect(@collection.fields).to eq(
+          {
+            __first__: @field,
+            __second__: @field,
+          }
+        )
+      end
+    end
+
+  end
+end
--- packages/forest_admin_datasource_toolkit/spec/spec_helper.rb
+++ packages/forest_admin_datasource_toolkit/spec/spec_helper.rb
@@ -1,13 +1,15 @@
-# frozen_string_literal: true
-
+require 'simplecov'
+require 'simplecov_json_formatter'
 require "forest_admin_datasource_toolkit"
+
+SimpleCov.formatter = SimpleCov::Formatter::JSONFormatter
+SimpleCov.start do
+  add_filter 'spec'
+end
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = ".rspec_status"
-
-  # Disable RSpec exposing methods globally on `Module` and `main`
-  config.disable_monkey_patching!
 
   config.expect_with :rspec do |c|
     c.syntax = :expect
