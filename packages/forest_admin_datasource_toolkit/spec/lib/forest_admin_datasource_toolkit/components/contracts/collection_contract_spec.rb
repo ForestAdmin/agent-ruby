505b815bae4efd453af1c4cdba9110ae50115e39
Nicolas Alexandre <nicolasalexandre9@gmail.com>
2023-09-25 14:35:03 +0200

    test: add tests on collection contract and datasource contract

---
+++ packages/forest_admin_datasource_toolkit/spec/lib/forest_admin_datasource_toolkit/components/contracts/collection_contract_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+
+module ForestAdminDatasourceToolkit
+  module Components
+    module Contracts
+      describe CollectionContract do
+        subject(:collection) { described_class.new }
+
+        it { expect { collection.datasource }.to raise_error(NotImplementedError) }
+        it { expect { collection.schema }.to raise_error(NotImplementedError) }
+        it { expect { collection.name }.to raise_error(NotImplementedError) }
+        it { expect { collection.execute }.to raise_error(NotImplementedError) }
+        it { expect { collection.form }.to raise_error(NotImplementedError) }
+        it { expect { collection.create }.to raise_error(NotImplementedError) }
+        it { expect { collection.list }.to raise_error(NotImplementedError) }
+        it { expect { collection.update }.to raise_error(NotImplementedError) }
+        it { expect { collection.delete }.to raise_error(NotImplementedError) }
+        it { expect { collection.aggregate }.to raise_error(NotImplementedError) }
+      end
+    end
+  end
+end
---
+++ packages/forest_admin_datasource_toolkit/spec/lib/forest_admin_datasource_toolkit/components/contracts/datasource_contract_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+module ForestAdminDatasourceToolkit
+  module Components
+    module Contracts
+      describe DatasourceContract do
+        subject(:datasource) { described_class.new }
+
+        it { expect { datasource.collections }.to raise_error(NotImplementedError) }
+        it { expect { datasource.charts }.to raise_error(NotImplementedError) }
+        it { expect { datasource.collection('__foo__') }.to raise_error(NotImplementedError) }
+        it { expect { datasource.add_collection('__collection__') }.to raise_error(NotImplementedError) }
+        it { expect { datasource.render_chart('caller', 'chart') }.to raise_error(NotImplementedError) }
+      end
+    end
+  end
+end
