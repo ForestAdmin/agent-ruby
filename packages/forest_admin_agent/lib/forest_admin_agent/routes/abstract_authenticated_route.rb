29deffe6b4c833dcddfc7467dd27451bb4e01efe
Matt <matthv@gmail.com>
2023-10-11 17:57:39 +0200

    chore: set ip to AbstractAuthenticatedRoute

--- packages/forest_admin_agent/lib/forest_admin_agent/routes/abstract_authenticated_route.rb
+++ packages/forest_admin_agent/lib/forest_admin_agent/routes/abstract_authenticated_route.rb
@@ -2,8 +2,8 @@ module ForestAdminAgent
   module Routes
     class AbstractAuthenticatedRoute < AbstractRoute
       def build(_args = {})
-        # args[:headers]['action_dispatch.remote_ip'].to_s
-        Facades::Whitelist.check_ip('127.0.0.1')
+        # TODO: handle call permissions
+        Facades::Whitelist.check_ip(args[:headers]['action_dispatch.remote_ip'].to_s)
       end
     end
   end
