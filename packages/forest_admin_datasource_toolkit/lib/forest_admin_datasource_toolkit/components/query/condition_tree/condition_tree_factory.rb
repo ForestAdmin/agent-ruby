1ed1a2a8792326b28b30befcbf66a3f6d16ab000
Matt <matthv@gmail.com>
2023-10-16 18:24:52 +0200

    feat(condition): prepare condition tree factory

---
+++ packages/forest_admin_datasource_toolkit/lib/forest_admin_datasource_toolkit/components/query/condition_tree/condition_tree_factory.rb
@@ -0,0 +1,126 @@
+module ForestAdminDatasourceToolkit
+  module Query
+    module ConditionTree
+      class ConditionTreeFactory
+        def self.match_records(collection, records)
+          ids = records.map { |record| ForestAdminDatasourceToolkit::Utils::Record.primary_keys(collection, record) }
+
+          match_ids(collection, ids)
+        end
+
+        def self.match_ids(collection, ids)
+          primary_key_names = ForestAdminDatasourceToolkit::Utils::Schema.primary_keys(collection)
+
+          raise ForestException, 'Collection must have at least one primary key' if primary_key_names.empty?
+
+          primary_key_names.each do |name|
+            operators = collection.fields[name].filter_operators
+
+            unless operators.include?('Equal') || operators.include?('In')
+              raise ForestException, "Field '#{name}' must support operators: ['Equal', 'In']"
+            end
+          end
+
+          match_fields(primary_key_names, ids)
+        end
+
+        def self.intersect(trees)
+          result = group('And', trees)
+          is_empty_and = result.is_a?(ConditionTreeBranch) && result.aggregator == 'And' && result.conditions.empty?
+
+          is_empty_and ? nil : result
+        end
+
+        def self.union(trees)
+          group('Or', trees)
+        end
+
+        def self.from_plain_object(json)
+          return nil if json.nil?
+
+          if leaf?(json)
+            ConditionTreeLeaf.new(json.field, json.operator, json.value)
+          elsif branch?(json)
+            branch = ConditionTreeBranch.new(
+              json.aggregator,
+              json.conditions.map { |sub_tree| from_plain_object(sub_tree) }
+            )
+
+            branch.conditions.length == 1 ? branch.conditions[0] : branch
+          else
+            raise ForestException, 'Failed to instantiate condition tree from json'
+          end
+        end
+
+        def self.group(aggregator, trees)
+          conditions = trees
+                       .filter { |tree| !tree.nil? }
+                       .reduce([]) do |current_conditions, tree|
+            if tree.is_a?(ConditionTreeBranch) && tree.aggregator == aggregator
+              current_conditions + tree.conditions
+            else
+              current_conditions + [tree]
+            end
+          end
+
+          conditions.length == 1 ? conditions[0] : ConditionTreeBranch.new(aggregator, conditions)
+        end
+
+        #   private static function matchFields(array $fields, array $values): ConditionTree
+        #     {
+        #         if (count($values) === 0) {
+        #             return new ConditionTreeBranch('Or', []);
+        #         }
+        #
+        #         if (count($fields) === 1) {
+        #             $fieldValues = [];
+        #             foreach ($values as $value) {
+        #                 $fieldValues[] = $value[0];
+        #             }
+        #
+        #             return count($fieldValues) > 1
+        #                 ? new ConditionTreeLeaf($fields[0], 'In', $fieldValues)
+        #                 : new ConditionTreeLeaf($fields[0], 'Equal', $fieldValues[0]);
+        #         }
+        #
+        #         $firstField = $fields[0];
+        #         unset($fields[0]);
+        #         $otherFields = array_values($fields);
+        #
+        #         $group = new IlluminateCollection();
+        #         foreach ($values as $value) {
+        #             $firstValue = $value[0];
+        #             unset($value[0]);
+        #             $otherValues = array_values($value);
+        #             if ($group->has($firstValue)) {
+        #                 $group->get($firstValue)->push($otherValues);
+        #             } else {
+        #                 $group->put($firstValue, collect([$otherValues]));
+        #             }
+        #         }
+        #
+        #         return self::union(
+        #             $group->map(
+        #                 fn ($subValues, $firstValue) => self::intersect(
+        #                     [
+        #                         self::matchFields([$firstField], [[$firstValue]]),
+        #                         self::matchFields($otherFields, $subValues->toArray()),
+        #                     ]
+        #                 )
+        #             )->all(),
+        #         );
+        #     }
+
+        def self.match_fields(fields, values); end
+
+        def self.leaf?(tree)
+          tree.key?('field') && tree.key?('operator') && (tree['operator'] == 'Present' || tree.key?('value'))
+        end
+
+        def self.branch?(tree)
+          tree.key?('aggregator') && tree.key?('conditions')
+        end
+      end
+    end
+  end
+end
