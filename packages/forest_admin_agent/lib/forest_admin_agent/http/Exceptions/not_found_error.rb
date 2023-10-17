f79d3810e974fa5a38237f7e98909ed680639bcd
Nicolas Alexandre <nicolasalexandre9@gmail.com>
2023-10-17 17:56:02 +0200

    feat(exception): update not found error

--- packages/forest_admin_agent/lib/forest_admin_agent/http/Exceptions/not_found_error.rb
+++ packages/forest_admin_agent/lib/forest_admin_agent/http/Exceptions/not_found_error.rb
@@ -2,8 +2,12 @@ module ForestAdminAgent
   module Http
     module Exceptions
       class NotFoundError < StandardError
+
+        attr_reader :name, :status
         def initialize(msg, name = 'NotFoundError')
           super msg
+          @name = name
+          @status = 404
         end
       end
     end
