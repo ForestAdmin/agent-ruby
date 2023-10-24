cf2bed134d0046a26ad1855e550a5be66a3721b8
Matt <matthv@gmail.com>
2023-10-24 09:38:54 +0200

    chore: add match method on leaf

--- packages/forest_admin_datasource_toolkit/lib/forest_admin_datasource_toolkit/components/query/condition_tree/nodes/condition_tree_leaf.rb
+++ packages/forest_admin_datasource_toolkit/lib/forest_admin_datasource_toolkit/components/query/condition_tree/nodes/condition_tree_leaf.rb
@@ -54,6 +54,45 @@ module ForestAdminDatasourceToolkit
             end
           end
 
+          def match(record, collection, timezone)
+            field_value = Record.field_value(record, @field)
+            column_type = Collection.get_field_schema(collection, @field).column_type
+            supported = %w[In Equal LessThan GreaterThan Match StartsWith EndsWith LongerThan ShorterThan IncludesAll
+                           NotIn NotEqual NotContains]
+
+            case @operator
+            when 'In'
+              Array(@value).include?(field_value)
+            when 'Equal'
+              field_value == @value
+            when 'LessThan'
+              field_value < @value
+            when 'GreaterThan'
+              field_value > @value
+            when 'Match'
+              field_value.is_a?(String) && field_value.match(@value)
+            when 'StartsWith'
+              field_value.is_a?(String) && field_value.start_with?(@value)
+            when 'EndsWith'
+              field_value.is_a?(String) && field_value.end_with?(@value)
+            when 'LongerThan'
+              field_value.is_a?(String) && field_value.length > @value
+            when 'ShorterThan'
+              field_value.is_a?(String) && field_value.length < @value
+            when 'IncludesAll'
+              Array(@value).all? { |v| field_value.include?(v) }
+            when 'NotIn', 'NotEqual', 'NotContains'
+              !inverse.match(record, collection, timezone)
+            else
+              ConditionTreeEquivalent.get_equivalent_tree(
+                self,
+                supported,
+                column_type,
+                timezone
+              )&.match(record, collection, timezone)
+            end
+          end
+
           def for_each_leaf
             yield(self)
           end
