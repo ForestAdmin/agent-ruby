4b63023f195069dfaedfe5e1ca826a98b4e319ca
Matt <matthv@gmail.com>
2023-10-26 14:23:07 +0200

    feat(condition): add pattern transforms

---
+++ packages/forest_admin_datasource_toolkit/lib/forest_admin_datasource_toolkit/components/query/condition_tree/transforms/pattern.rb
@@ -0,0 +1,48 @@
+module ForestAdminDatasourceToolkit
+  module Components
+    module Query
+      module ConditionTree
+        module Transforms
+          class Pattern
+            def self.likes(get_pattern, case_sensitive)
+              operator = case_sensitive ? Operators::LIKE : Operators::ILIKE
+
+              {
+                dependsOn: [operator],
+                forTypes: ['String'],
+                replacer: ->(leaf) { leaf.override(operator: operator, value: get_pattern.call(leaf.value)) }
+              }
+            end
+
+            def self.match(case_sensitive)
+              {
+                dependsOn: [Operators::MATCH],
+                forTypes: ['String'],
+                replacer: lambda { |leaf|
+                  regex = leaf.value.gsub(/([\.\\\+\*\?\[\^\]\$\(\)\{\}\=\!\<\>\|\:\-])/, '\\\\\1')
+                  regex.tr!(/%/, '.*')
+                  regex.tr!('_', '.')
+
+                  leaf.override(operator: Operators::MATCH, value: /^#{regex}$/ + (case_sensitive ? '' : 'i'))
+                }
+              }
+            end
+
+            def self.pattern_transforms
+              {
+                Operators::CONTAINS => [likes(->(value) { "%#{value}%" }, true)],
+                Operators::STARTS_WITH => [likes(->(value) { "#{value}%" }, true)],
+                Operators::ENDS_WITH => [likes(->(value) { "%#{value}" }, true)],
+                Operators::I_CONTAINS => [likes(->(value) { "%#{value}%" }, false)],
+                Operators::I_STARTS_WITH => [likes(->(value) { "#{value}%" }, false)],
+                Operators::I_ENDS_WITH => [likes(->(value) { "%#{value}" }, false)],
+                Operators::I_LIKE => [match(false)],
+                Operators::LIKE => [match(true)]
+              }
+            end
+          end
+        end
+      end
+    end
+  end
+end
