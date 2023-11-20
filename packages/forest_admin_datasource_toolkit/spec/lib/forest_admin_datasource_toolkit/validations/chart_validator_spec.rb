f53709cc386fc0d915079ab7f0f00706c92b1dd4
Matt <matthv@gmail.com>
2023-11-20 10:55:29 +0100

    chore: add test on chart validator class

---
+++ packages/forest_admin_datasource_toolkit/spec/lib/forest_admin_datasource_toolkit/validations/chart_validator_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+
+module ForestAdminDatasourceToolkit
+  module Validations
+    describe ChartValidator do
+      context 'when the condition is true' do
+        it 'raises an exception' do
+          expect do
+            described_class.validate?(true, { key1: 1, key2: 2 }, 'key1,key2')
+          end.to raise_error(ForestAdminDatasourceToolkit::Exceptions::ForestException,
+                             "🌳🌳🌳 The result columns must be named 'key1,key2' instead of 'key1,key2'")
+        end
+      end
+
+      context 'when the condition is false' do
+        it 'returns true' do
+          expect(described_class.validate?(false, { key1: 1, key2: 2 }, 'key1,key2')).to be true
+        end
+      end
+    end
+  end
+end
