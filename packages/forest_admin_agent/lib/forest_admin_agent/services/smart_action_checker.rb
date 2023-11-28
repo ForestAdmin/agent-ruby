5ac18a0c7cbf3d479b5bbc1e4e30846c8e7ede1b
Matt <matthv@gmail.com>
2023-11-28 17:00:35 +0100

    refactor: smart_action_checker

--- packages/forest_admin_agent/lib/forest_admin_agent/services/smart_action_checker.rb
+++ packages/forest_admin_agent/lib/forest_admin_agent/services/smart_action_checker.rb
@@ -10,16 +10,18 @@ module ForestAdminAgent
 
       INVALID_ACTION_CONDITION_ERROR = 'InvalidActionConditionError'.freeze
 
-      def initialize(parameters, collection, smart_action, user)
+      def initialize(parameters, collection, smart_action, caller, role_id, filter)
         @parameters = parameters
-        @attributes = @parameters[:data][:attributes]
         @collection = collection
         @smart_action = smart_action
-        @user = user
+        @caller = caller
+        @role_id = role_id
+        @filter = filter
+        @attributes = parameters[:data][:attributes]
       end
 
       def can_execute?
-        if @attributes[:signed_approval_request].present? &&
+        if @attributes[:signed_approval_request] &&
            @smart_action['userApprovalEnabled'].include?(@user['roleId'])
           can_approve?
         else
@@ -30,10 +32,9 @@ module ForestAdminAgent
       private
 
       def can_approve?
-        @parameters = RequestPermission.decodeSignedApprovalRequest(@parameters)
-        if (@smart_action['userApprovalConditions'].empty? || match_conditions('userApprovalConditions')) &&
-           (@attributes[:requester_id] != @user['id'] || @smart_action['selfApprovalEnabled'].include?(@user['roleId']))
-
+        if (@smart_action.include?(@role_id) &&
+           (@smart_action['userApprovalConditions'].empty? || match_conditions('userApprovalConditions'))) &&
+           (@attributes[:requester_id] != @caller.id || @smart_action['selfApprovalEnabled'].include?(@role_id))
           return true
         end
 
@@ -41,17 +42,19 @@ module ForestAdminAgent
       end
 
       def can_trigger?
-        if @smart_action['triggerEnabled'].include?(@user['roleId']) &&
-           @smart_action['approvalRequired'].exclude?(@user['roleId'])
-          return true if @smart_action['triggerConditions'].empty? || match_conditions('triggerConditions')
-        elsif @smart_action['approvalRequired'].include?(@user['roleId'])
+        if @smart_action.include?(@role_id) && !@smart_action['approvalRequired'].include?(@role_id)
+          true if @smart_action['triggerConditions'].empty? || match_conditions('triggerConditions')
+        elsif @smart_action['approvalRequired'].include?(@role_id) && @smart_action['triggerEnabled'].include?(@role_id)
           if @smart_action['approvalRequiredConditions'].empty? || match_conditions('approvalRequiredConditions')
-            raise RequireApproval, @smart_action['userApprovalEnabled']
+            raise RequireApprovalError.new(
+              'This action requires to be approved.',
+              'CustomActionRequiresApprovalError',
+              @smart_action['userApprovalEnabled']
+            )
+          elsif @smart_action['triggerConditions'].empty? || match_conditions('triggerConditions')
+            true
           end
-          return true if @smart_action['triggerConditions'].empty? || match_conditions('triggerConditions')
         end
-
-        raise ForbiddenError.new('You don\'t have the permission to trigger this action.', TRIGGER_FORBIDDEN_ERROR)
       end
 
       def match_conditions(condition_name)
