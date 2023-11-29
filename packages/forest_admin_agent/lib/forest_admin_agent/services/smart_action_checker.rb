f742585bcac48fde286355c31fb894015010cd8b
Matt <matthv@gmail.com>
2023-11-29 15:41:20 +0100

    chore: add getter on smart_action_checker

--- packages/forest_admin_agent/lib/forest_admin_agent/services/smart_action_checker.rb
+++ packages/forest_admin_agent/lib/forest_admin_agent/services/smart_action_checker.rb
@@ -7,6 +7,8 @@ module ForestAdminAgent
       include ForestAdminDatasourceToolkit::Components::Query
       include ForestAdminDatasourceToolkit::Components::Query::ConditionTree
 
+      attr_reader :parameters, :collection, :smart_action, :caller, :role_id, :filter, :attributes
+
       TRIGGER_FORBIDDEN_ERROR = 'CustomActionTriggerForbiddenError'.freeze
 
       REQUIRE_APPROVAL_ERROR = 'CustomActionRequiresApprovalError'.freeze
@@ -24,7 +26,7 @@ module ForestAdminAgent
       end
 
       def can_execute?
-        if @attributes[:signed_approval_request].nil?
+        if attributes[:signed_approval_request].nil?
           can_trigger?
         else
           can_approve?
@@ -34,9 +36,9 @@ module ForestAdminAgent
       private
 
       def can_approve?
-        if @smart_action['userApprovalEnabled'].include?(@role_id) &&
-           (@smart_action['userApprovalConditions'].empty? || match_conditions('userApprovalConditions')) &&
-           (@attributes[:requester_id] != @caller.id || @smart_action['selfApprovalEnabled'].include?(@role_id))
+        if smart_action['userApprovalEnabled'].include?(role_id) &&
+           (smart_action['userApprovalConditions'].empty? || match_conditions('userApprovalConditions')) &&
+           (attributes[:requester_id] != caller.id || smart_action['selfApprovalEnabled'].include?(role_id))
           return true
         end
 
@@ -44,16 +46,16 @@ module ForestAdminAgent
       end
 
       def can_trigger?
-        if @smart_action['triggerEnabled'].include?(@role_id) && !@smart_action['approvalRequired'].include?(@role_id)
-          return true if @smart_action['triggerConditions'].empty? || match_conditions('triggerConditions')
-        elsif @smart_action['approvalRequired'].include?(@role_id) && @smart_action['triggerEnabled'].include?(@role_id)
-          if @smart_action['approvalRequiredConditions'].empty? || match_conditions('approvalRequiredConditions')
+        if smart_action['triggerEnabled'].include?(role_id) && !smart_action['approvalRequired'].include?(role_id)
+          return true if smart_action['triggerConditions'].empty? || match_conditions('triggerConditions')
+        elsif smart_action['approvalRequired'].include?(role_id) && smart_action['triggerEnabled'].include?(role_id)
+          if smart_action['approvalRequiredConditions'].empty? || match_conditions('approvalRequiredConditions')
             raise RequireApproval.new(
               'This action requires to be approved.',
               REQUIRE_APPROVAL_ERROR,
-              @smart_action['userApprovalEnabled']
+              smart_action['userApprovalEnabled']
             )
-          elsif @smart_action['triggerConditions'].empty? || match_conditions('triggerConditions')
+          elsif smart_action['triggerConditions'].empty? || match_conditions('triggerConditions')
             return true
           end
         end
@@ -62,27 +64,27 @@ module ForestAdminAgent
       end
 
       def match_conditions(condition_name)
-        pk = Schema.primary_keys(@collection)[0]
-        condition_filter = if @attributes[:all_records]
+        pk = Schema.primary_keys(collection)[0]
+        condition_filter = if attributes[:all_records]
                              puts 1
-                             Nodes::ConditionTreeLeaf.new(pk, 'NOT_EQUAL', @attributes[:all_records_ids_excluded])
+                             Nodes::ConditionTreeLeaf.new(pk, 'NOT_EQUAL', attributes[:all_records_ids_excluded])
                            else
-                             Nodes::ConditionTreeLeaf.new(pk, 'IN', @attributes[:ids])
+                             Nodes::ConditionTreeLeaf.new(pk, 'IN', attributes[:ids])
                            end
 
-        condition = @smart_action[condition_name][0]['filter']
-        conditional_filter = @filter.override(
+        condition = smart_action[condition_name][0]['filter']
+        conditional_filter = filter.override(
           condition_tree: ConditionTreeFactory.intersect(
             [
-              ConditionTreeParser.from_plain_object(@collection, condition),
-              @filter.condition_tree,
+              ConditionTreeParser.from_plain_object(collection, condition),
+              filter.condition_tree,
               condition_filter
             ]
           )
         )
-        rows = @collection.aggregate(@caller, conditional_filter, Aggregation.new(operation: 'Count'))
+        rows = collection.aggregate(caller, conditional_filter, Aggregation.new(operation: 'Count'))
 
-        (rows[0]['value'] || 0) == @attributes[:ids].count
+        (rows[0]['value'] || 0) == attributes[:ids].count
       rescue StandardError
         raise ConflictError.new(
           'The conditions to trigger this action cannot be verified. Please contact an administrator.',
