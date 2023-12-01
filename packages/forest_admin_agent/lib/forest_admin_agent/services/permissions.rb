44c47d9095dc6fc0c3186daafcdd69f5db798019
Matt <matthv@gmail.com>
2023-12-01 16:24:26 +0100

    fix(permissions): remove all string to sym

--- packages/forest_admin_agent/lib/forest_admin_agent/services/permissions.rb
+++ packages/forest_admin_agent/lib/forest_admin_agent/services/permissions.rb
@@ -2,6 +2,7 @@ module ForestAdminAgent
   module Services
     class Permissions
       include ForestAdminAgent::Http::Exceptions
+      include ForestAdminAgent::Utils
       include ForestAdminDatasourceToolkit::Exceptions
       include ForestAdminDatasourceToolkit::Components::Query::ConditionTree
 
@@ -33,12 +34,12 @@ module ForestAdminAgent
         user_data = get_user_data(caller.id)
         collections_data = get_collections_permissions_data(force_fetch: allow_fetch)
 
-        is_allowed = collections_data.key?(collection.name) && collections_data[collection.name][action].include?(user_data['roleId'])
+        is_allowed = collections_data.key?(collection.name.to_sym) && collections_data[collection.name.to_sym][action].include?(user_data[:roleId])
 
         # Refetch
         unless is_allowed
           collections_data = get_collections_permissions_data(force_fetch: true)
-          is_allowed = collections_data[collection.name][action].include?(user_data['roleId'])
+          is_allowed = collections_data[collection.name.to_sym][action].include?(user_data[:roleId])
         end
 
         # still not allowed - throw forbidden message
@@ -49,8 +50,7 @@ module ForestAdminAgent
 
       def can_chart?(parameters)
         attributes = sanitize_chart_parameters(parameters)
-
-        hash_request = "#{attributes["type"]}:#{array_hash(attributes)}"
+        hash_request = "#{attributes[:type]}:#{array_hash(attributes)}"
         is_allowed = get_chart_data(caller.rendering_id).include?(hash_request)
 
         # Refetch
@@ -73,44 +73,45 @@ module ForestAdminAgent
         return true unless permission_system?
 
         user_data = get_user_data(caller.id)
-        collections_data = get_collections_permissions_data(allow_fetch)
-        action = find_action_from_endpoint(collection.name, request.path_info, request.method)
+        collections_data = get_collections_permissions_data(force_fetch: allow_fetch)
+        action = find_action_from_endpoint(collection.name.to_sym, request.path_info, request.method)
 
-        raise ForestException, "The collection #{collection.name} does not have this smart action" if action.nil?
+        raise ForestException, "The collection #{collection.name.to_sym} does not have this smart action" if action.nil?
 
         smart_action_approval = SmartActionChecker.new(
           request,
           collection,
-          collections_data[collection.name]['actions'][action['name']],
+          collections_data[collection.name.to_sym][:actions][action[:name]],
           caller,
-          user_data['roleId'],
+          user_data[:roleId],
           filter
         )
 
         smart_action_approval.can_execute?
         # TODO: HANDLE LOGGER
-        # logger.debug("User #{user_data['roleId']} is #{is_allowed ? '' : 'not'} allowed to perform #{action['name']}")
+        # logger.debug("User #{user_data[:roleId]} is #{is_allowed ? '' : 'not'} allowed to perform #{action['name']}")
       end
 
       def get_scope(collection)
         permissions = get_scope_and_team_data(caller.rendering_id)
-        scope = permissions['scopes'][collection.name]
-        team = permissions['team']
+        scope = permissions[:scopes][collection.name.to_sym]
+        team = permissions[:team]
         user = get_user_data(caller.id)
 
         return nil if scope.nil?
 
-        context_variables = ContextVariables.new(team: team, user: user)
+        context_variables = ContextVariables.new(team, user)
 
         ContextVariablesInjector.inject_context_in_filter(scope, context_variables)
       end
 
       def get_user_data(user_id)
         cache.get('forest.users') do
-          response = forest_api.fetch('/liana/v4/permissions/users')
+          response = fetch('/liana/v4/permissions/users')
           users = {}
+
           response.each do |user|
-            users[user['id'].to_s] = user
+            users[user[:id].to_s] = user
           end
 
           # TODO: HANDLE LOGGER
@@ -123,20 +124,20 @@ module ForestAdminAgent
       def get_team(rendering_id)
         permissions = get_scope_and_team_data(rendering_id)
 
-        permissions['team']
+        permissions[:team]
       end
 
       private
 
       def get_collections_permissions_data(force_fetch: false)
-        invalidate_cache('forest.collections') if force_fetch == true
+        self.class.invalidate_cache('forest.collections') if force_fetch == true
 
         cache.get('forest.collections') do
-          response = forest_api.fetch('/liana/v4/permissions/environment')
+          response = fetch('/liana/v4/permissions/environment')
           collections = {}
-          response['collections'].each do |name, collection|
-            collections[name] =
-              format_collection_crud_permission(collection).merge!(format_collection_action_permission(collection))
+
+          response[:collections].each do |name, collection|
+            collections[name] = decode_crud_permissions(collection).merge(decode_action_permissions(collection))
           end
 
           # TODO: HANDLE LOGGER
@@ -147,14 +148,14 @@ module ForestAdminAgent
       end
 
       def get_chart_data(rendering_id, force_fetch: false)
-        invalidate_cache('forest.stats') if force_fetch == true
+        self.class.invalidate_cache('forest.stats') if force_fetch == true
 
         cache.get('forest.stats') do
-          response = forest_api.fetch("/liana/v4/permissions/renderings/#{rendering_id}")
+          response = fetch("/liana/v4/permissions/renderings/#{rendering_id}")
           stat_hash = []
-          response['stats'].each do |stat|
+          response[:stats].each do |stat|
             stat = stat.select { |_, value| !value.nil? && value != '' }
-            stat_hash << "#{stat["type"]}:#{array_hash(stat)}"
+            stat_hash << "#{stat[:type]}:#{array_hash(stat)}"
           end
 
           # TODO: HANDLE LOGGER
@@ -165,10 +166,10 @@ module ForestAdminAgent
       end
 
       def sanitize_chart_parameters(parameters)
-        parameters = parameters.to_h
-        parameters.delete('timezone')
-        parameters.delete('collection')
-        parameters.delete('contextVariables')
+        # parameters = parameters.to_h
+        parameters.delete(:timezone)
+        parameters.delete(:collection)
+        parameters.delete(:contextVariables)
 
         parameters.select { |_, value| !value.nil? && value != '' }
       end
@@ -178,20 +179,21 @@ module ForestAdminAgent
       #   ArrayHelper::ksortRecursive($data);
       #
       # return sha1(json_encode($data, JSON_THROW_ON_ERROR));
+      # stat_hash << "#{stat['type']}:#{Digest::SHA1.hexdigest(stat.sort.to_h.to_s)}"
       # }
       def array_hash(data)
-        data.deep_sort.to_s
+        # data.deep_sort.to_s
 
-        Digest::SHA1.hexdigest(data.to_json)
+        Digest::SHA1.hexdigest(data.sort.to_h.to_s)
       end
 
       def get_scope_and_team_data(rendering_id)
         cache.get('forest.scopes') do
           data = {}
-          response = forest_api.fetch("/liana/v4/permissions/renderings/#{rendering_id}")
+          response = fetch("/liana/v4/permissions/renderings/#{rendering_id}")
 
-          data['scopes'] = decode_scope_permissions(response['collections'])
-          data['team'] = response['team']
+          data[:scopes] = decode_scope_permissions(response[:collections])
+          data[:team] = response[:team]
 
           data
         end
@@ -199,8 +201,7 @@ module ForestAdminAgent
 
       def permission_system?
         cache.get('forest.has_permission') do
-          response = forest_api.fetch('/liana/v4/permissions/environment')
-
+          response = fetch('/liana/v4/permissions/environment')
           !response.nil?
         end
       end
@@ -215,27 +216,27 @@ module ForestAdminAgent
 
       def decode_crud_permissions(collection)
         {
-          'browse' => collection['collection']['browseEnabled']['roles'],
-          'read' => collection['collection']['readEnabled']['roles'],
-          'edit' => collection['collection']['editEnabled']['roles'],
-          'add' => collection['collection']['addEnabled']['roles'],
-          'delete' => collection['collection']['deleteEnabled']['roles'],
-          'export' => collection['collection']['exportEnabled']['roles']
+          browse: collection[:collection][:browseEnabled][:roles],
+          read: collection[:collection][:readEnabled][:roles],
+          edit: collection[:collection][:editEnabled][:roles],
+          add: collection[:collection][:addEnabled][:roles],
+          delete: collection[:collection][:deleteEnabled][:roles],
+          export: collection[:collection][:exportEnabled][:roles]
         }
       end
 
-      def decode_acction_permissions(collection)
+      def decode_action_permissions(collection)
         actions = {}
         actions[:actions] = {}
-        collection['actions'].each do |id, action|
+        collection[:actions].each do |id, action|
           actions[:actions][id] = {
-            'triggerEnabled' => action['triggerEnabled']['roles'],
-            'triggerConditions' => action['triggerConditions'],
-            'approvalRequired' => action['approvalRequired']['roles'],
-            'approvalRequiredConditions' => action['approvalRequiredConditions'],
-            'userApprovalEnabled' => action['userApprovalEnabled']['roles'],
-            'userApprovalConditions' => action['userApprovalConditions'],
-            'selfApprovalEnabled' => action['selfApprovalEnabled']['roles']
+            triggerEnabled: action[:triggerEnabled][:roles],
+            triggerConditions: action[:triggerConditions],
+            approvalRequired: action[:approvalRequired][:roles],
+            approvalRequiredConditions: action[:approvalRequiredConditions],
+            userApprovalEnabled: action[:userApprovalEnabled][:roles],
+            userApprovalConditions: action[:userApprovalConditions],
+            selfApprovalEnabled: action[:selfApprovalEnabled][:roles]
           }
         end
 
@@ -245,16 +246,16 @@ module ForestAdminAgent
       def decode_scope_permissions(raw_permissions)
         scopes = {}
         raw_permissions.each do |collection_name, value|
-          scopes[collection_name] = ConditionTreeFactory.from_plain_object(value['scope']) unless value['scope'].nil?
+          scopes[collection_name] = ConditionTreeFactory.from_plain_object(value[:scope]) unless value[:scope].nil?
         end
 
         scopes
       end
 
       def fetch(url)
-        response = forest_api.get(url, {})
+        response = forest_api.get(url)
 
-        JSON.parse(response.body)
+        JSON.parse(response.body, symbolize_names: true)
       rescue StandardError => e
         forest_api.handle_response_error(e)
       end
