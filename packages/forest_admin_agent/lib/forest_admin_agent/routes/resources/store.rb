ac217e703fb938dd27586403b75141863023b3b6
Matt <matthv@gmail.com>
2023-12-12 12:19:07 +0100

    fix: lint

--- packages/forest_admin_agent/lib/forest_admin_agent/routes/resources/store.rb
+++ packages/forest_admin_agent/lib/forest_admin_agent/routes/resources/store.rb
@@ -33,17 +33,17 @@ module ForestAdminAgent
         def link_one_to_one_relations(args, record)
           args[:params][:data][:relationships]&.map do |field, value|
             schema = @collection.fields[field]
-            if schema.type == 'OneToOne'
-              id = Utils::Id.unpack_id(@collection, value['data']['id'], with_key: true)
-              foreign_collection = @datasource.collection(schema.foreign_collection)
-              # Load the value that will be used as origin_key
-              origin_value = record[schema.origin_key_target]
+            next unless schema.type == 'OneToOne'
 
-              # update new relation (may update zero or one records).
-              condition_tree = ConditionTree::ConditionTreeFactory.match_records(foreign_collection, [id])
-              filter = Filter.new(condition_tree: condition_tree)
-              foreign_collection.update(@caller, filter, { schema.origin_key => origin_value })
-            end
+            id = Utils::Id.unpack_id(@collection, value['data']['id'], with_key: true)
+            foreign_collection = @datasource.collection(schema.foreign_collection)
+            # Load the value that will be used as origin_key
+            origin_value = record[schema.origin_key_target]
+
+            # update new relation (may update zero or one records).
+            condition_tree = ConditionTree::ConditionTreeFactory.match_records(foreign_collection, [id])
+            filter = Filter.new(condition_tree: condition_tree)
+            foreign_collection.update(@caller, filter, { schema.origin_key => origin_value })
           end
         end
       end
