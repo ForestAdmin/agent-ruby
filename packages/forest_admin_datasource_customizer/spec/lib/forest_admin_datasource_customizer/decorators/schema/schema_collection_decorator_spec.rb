1b450573f356df49728da1c576caf09a539ea162
Matt <matthv@gmail.com>
2023-12-18 18:03:52 +0100

    chore: add test for schema collection

---
+++ packages/forest_admin_datasource_customizer/spec/lib/forest_admin_datasource_customizer/decorators/schema/schema_collection_decorator_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+
+module ForestAdminDatasourceToolkit
+  module Decorators
+    module Schema
+      describe SchemaCollectionDecorator do
+        subject(:schema_collection_decorator) { described_class }
+
+        it 'overwrites fields from the schema' do
+          collection = ForestAdminDatasourceToolkit::Collection.new(nil, 'test')
+          collection.schema[:countable] = true
+
+          decorator = schema_collection_decorator.new(collection, nil)
+          decorator.override_schema(countable: false)
+
+          expect(collection.schema[:countable]).to be true
+          expect(decorator.schema[:countable]).to be false
+        end
+      end
+    end
+  end
+end
