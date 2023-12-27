7f97f86bcf3f797c81e6e7f48a28a497bfb13eca
Matt <matthv@gmail.com>
2023-12-27 10:07:06 +0100

    fix: list test (add decorators attributes on response)

--- packages/forest_admin_agent/lib/forest_admin_agent/routes/resources/list.rb
+++ packages/forest_admin_agent/lib/forest_admin_agent/routes/resources/list.rb
@@ -15,7 +15,6 @@ module ForestAdminAgent
         def handle_request(args = {})
           build(args)
           @permissions.can?(:browse, @collection)
-
           filter = ForestAdminDatasourceToolkit::Components::Query::Filter.new(
             condition_tree: ConditionTreeFactory.intersect([
                                                              @permissions.get_scope(@collection),
@@ -23,7 +22,9 @@ module ForestAdminAgent
                                                                @collection, args
                                                              )
                                                            ]),
-            page: ForestAdminAgent::Utils::QueryStringParser.parse_pagination(args)
+            page: ForestAdminAgent::Utils::QueryStringParser.parse_pagination(args),
+            search: ForestAdminAgent::Utils::QueryStringParser.parse_search(@collection, args),
+            search_extended: ForestAdminAgent::Utils::QueryStringParser.parse_search_extended(args)
           )
           projection = ForestAdminAgent::Utils::QueryStringParser.parse_projection_with_pks(@collection, args)
           records = @collection.list(@caller, filter, projection)
@@ -34,9 +35,26 @@ module ForestAdminAgent
               records,
               is_collection: true,
               serializer: Serializer::ForestSerializer,
-              include: projection.relations.keys
+              include: projection.relations.keys,
+              meta: handle_search_decorator(args[:params]['search'], records)
             )
           }
+        end
+
+        def handle_search_decorator(search_value, records)
+          decorator = { decorators: [] }
+          unless search_value.nil?
+            records.each_with_index do |record, index|
+              decorator[:decorators][index] = { id: Utils::Id.pack_id(@collection, record), search: [] }
+              record.attributes.each do |field_key, field_value|
+                if !field_value.is_a?(Array) && field_value.to_s.downcase.include?(search_value.downcase)
+                  decorator[:decorators][index][:search] << field_key
+                end
+              end
+            end
+          end
+
+          decorator
         end
       end
     end
