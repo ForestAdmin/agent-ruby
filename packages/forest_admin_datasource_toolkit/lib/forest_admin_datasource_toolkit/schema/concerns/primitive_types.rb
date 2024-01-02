121d912c691f57afcaaa3b0e4a15b91e59e6c0cf
Matt <matthv@gmail.com>
2024-01-02 19:36:33 +0100

    chore: add primitive type

---
+++ packages/forest_admin_datasource_toolkit/lib/forest_admin_datasource_toolkit/schema/concerns/primitive_types.rb
@@ -0,0 +1,19 @@
+module ForestAdminDatasourceToolkit
+  module Schema
+    module Concerns
+      class PrimitiveTypes
+        BINARY = 'Binary'.freeze
+        BOOLEAN = 'Boolean'.freeze
+        DATE = 'Date'.freeze
+        DATE_ONLY = 'Dateonly'.freeze
+        ENUM = 'Enum'.freeze
+        JSON = 'Json'.freeze
+        NUMBER = 'Number'.freeze
+        POINT = 'Point'.freeze
+        STRING = 'String'.freeze
+        TIME_ONLY = 'Timeonly'.freeze
+        UUID = 'Uuid'.freeze
+      end
+    end
+  end
+end
