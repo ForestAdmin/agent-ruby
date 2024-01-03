a058c15bd3b5c2cf006aa03961a5b955246e5b80
Nicolas Alexandre <nicolasalexandre9@gmail.com>
2024-01-03 15:46:37 +0100

    feat(decorator): add dynamic field

---
+++ packages/forest_admin_datasource_customizer/lib/forest_admin_datasource_customizer/decorators/actions/dynamic_field.rb
@@ -0,0 +1,38 @@
+module ForestAdminDatasourceCustomizer
+  module Decorators
+    module Actions
+      class DynamicField
+        attr_accessor :type, :label, :description, :is_required, :is_read_only, :if_condition, :value, :default_value,
+                      :collection_name, :enum_values
+
+        def initialize(
+          type:,
+          label:,
+          description: nil,
+          is_required: false,
+          is_read_only: false,
+          if_condition: nil,
+          value: nil,
+          default_value: nil,
+          collection_name: nil,
+          enum_values: nil
+        )
+          @type = type
+          @label = label
+          @description = description
+          @is_required = is_required
+          @is_read_only = is_read_only
+          @if_condition = if_condition
+          @value = value
+          @default_value = default_value
+          @collection_name = collection_name
+          @enum_values = enum_values
+        end
+
+        def static?
+          instance_variables.all? { |attribute| !attribute.respond_to?(:call) }
+        end
+      end
+    end
+  end
+end
