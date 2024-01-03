373522d0aa2109ab7855afe60c51d32d5783b3b1
Nicolas Alexandre <nicolasalexandre9@gmail.com>
2024-01-03 15:45:08 +0100

    feat(decorator): add scope and field types

---
+++ packages/forest_admin_datasource_customizer/lib/forest_admin_datasource_customizer/decorators/actions/types/action_scope.rb
@@ -0,0 +1,15 @@
+module ForestAdminDatasourceCustomizer
+  module Decorators
+    module Actions
+      module Types
+        module ActionScope
+          SINGLE = 'single'.freeze
+
+          SCOPE = 'scope'.freeze
+
+          GLOBAL = 'global'.freeze
+        end
+      end
+    end
+  end
+end
---
+++ packages/forest_admin_datasource_customizer/lib/forest_admin_datasource_customizer/decorators/actions/types/field_type.rb
@@ -0,0 +1,35 @@
+module ForestAdminDatasourceCustomizer
+  module Decorators
+    module Actions
+      module Types
+        module FieldType
+          BOOLEAN = 'Boolean'.freeze
+
+          DATE = 'Date'.freeze
+
+          DATE_ONLY = 'Dateonly'.freeze
+
+          COLLECTION = 'Collection'.freeze
+
+          ENUM = 'Enum'.freeze
+
+          ENUM_LIST = 'EnumList'.freeze
+
+          FILE = 'File'.freeze
+
+          FILE_LIST = 'FileList'.freeze
+
+          JSON = 'Json'.freeze
+
+          NUMBER = 'Number'.freeze
+
+          NUMBER_LIST = 'NumberList'.freeze
+
+          STRING = 'String'.freeze
+
+          STRING_LIST = 'StringList'.freeze
+        end
+      end
+    end
+  end
+end
