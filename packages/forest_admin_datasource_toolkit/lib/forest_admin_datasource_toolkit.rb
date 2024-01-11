d98e60e8b20dbc7539de789c7714b1f4c8e171c6
Matt <matthv@gmail.com>
2024-01-11 11:38:45 +0100

    chore: setup datasourcetoolkit

--- packages/forest_admin_datasource_toolkit/lib/forest_admin_datasource_toolkit.rb
+++ packages/forest_admin_datasource_toolkit/lib/forest_admin_datasource_toolkit.rb
@@ -2,6 +2,7 @@ require_relative "forest_admin_datasource_toolkit/version"
 require 'zeitwerk'
 
 loader = Zeitwerk::Loader.for_gem
+loader.ignore("#{__dir__}/spec")
 loader.setup
 
 module ForestAdminDatasourceToolkit
