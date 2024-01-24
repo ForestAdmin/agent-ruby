59c25ced431c3af01d41e4c34f06946324dab7f8
Matt <matthv@gmail.com>
2024-01-24 11:25:06 +0100

    chore: update config

--- packages/forest_admin_datasource_toolkit/lib/forest_admin_datasource_toolkit.rb
+++ packages/forest_admin_datasource_toolkit/lib/forest_admin_datasource_toolkit.rb
@@ -2,7 +2,6 @@ require_relative "forest_admin_datasource_toolkit/version"
 require 'zeitwerk'
 
 loader = Zeitwerk::Loader.for_gem
-loader.ignore("#{__dir__}/spec")
 loader.setup
 
 module ForestAdminDatasourceToolkit
