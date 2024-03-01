f35a6b116e124e50452588c259210fd683be1a63
Nicolas Alexandre <nicolasalexandre9@gmail.com>
2024-03-01 14:26:20 +0100

    fix: lint

--- packages/forest_admin_datasource_active_record/spec/forest_admin_datasource_active_record_spec.rb
+++ packages/forest_admin_datasource_active_record/spec/forest_admin_datasource_active_record_spec.rb
@@ -1,5 +1,5 @@
 RSpec.describe ForestAdminDatasourceActiveRecord do
   it 'has a version number' do
-    expect(ForestAdminDatasourceActiveRecord::VERSION).not_to be_nil
+    expect(described_class::VERSION).not_to be_nil
   end
 end
--- packages/forest_admin_datasource_toolkit/spec/forest_admin_datasource_toolkit_spec.rb
+++ packages/forest_admin_datasource_toolkit/spec/forest_admin_datasource_toolkit_spec.rb
@@ -1,5 +1,5 @@
 RSpec.describe ForestAdminDatasourceToolkit do
   it 'has a version number' do
-    expect(ForestAdminDatasourceToolkit::VERSION).not_to be_nil
+    expect(described_class::VERSION).not_to be_nil
   end
 end
