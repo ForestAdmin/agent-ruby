cdaf925cb6c54a5ebf9997364f93a4e4ff967332
Matt <matthv@gmail.com>
2024-03-19 15:38:58 +0100

    chore: add test on datasource_customizer

---
+++ packages/forest_admin_datasource_customizer/spec/lib/forest_admin_datasource_customizer/datasource_customizer_spec.rb
@@ -0,0 +1,33 @@
+require 'spec_helper'
+
+module ForestAdminDatasourceCustomizer
+  include ForestAdminDatasourceToolkit::Schema
+
+  describe DatasourceCustomizer do
+    let(:datasource) { ForestAdminDatasourceToolkit::Datasource.new }
+
+    before do
+      @collection = instance_double(
+        ForestAdminDatasourceToolkit::Collection,
+        name: 'collection',
+        schema: {
+          fields: {
+            'id' => ColumnSchema.new(column_type: 'Number', is_primary_key: true)
+          }
+        }
+      )
+
+      datasource.add_collection(@collection)
+    end
+
+    context 'when removing a collection' do
+      it 'removes the collection into the datasource' do
+        customized = described_class.new
+                                    .add_datasource(datasource, {})
+                                    .remove_collection('collection')
+
+        expect(customized.collections).to be_empty
+      end
+    end
+  end
+end
