7564441c7cff385f105766d036498b7f1021ea95
Matt <matthv@gmail.com>
2024-03-20 18:13:53 +0100

    chore: add test on record_validator utils

--- packages/forest_admin_datasource_toolkit/lib/forest_admin_datasource_toolkit/validations/record_validator.rb
+++ packages/forest_admin_datasource_toolkit/lib/forest_admin_datasource_toolkit/validations/record_validator.rb
@@ -4,21 +4,21 @@ module ForestAdminDatasourceToolkit
       include ForestAdminDatasourceToolkit::Exceptions
 
       def self.validate(collection, record_data)
-        raise ForestException, 'The record data is empty' if !record_data || record_data.keys.empty?
+        raise ForestException, 'The record data is empty' if !record_data || record_data.empty?
 
         record_data.each_key do |key|
           schema = collection.schema[:fields][key]
 
           if !schema
             raise ForestException, "Unknown field #{key}"
-          elsif schema[:type] == 'Column'
+          elsif schema.type == 'Column'
             FieldValidator.validate(collection, key, record_data[key])
-          elsif schema[:type] == 'OneToOne' || schema[:type] == 'ManyToOne'
+          elsif schema.type == 'OneToOne' || schema.type == 'ManyToOne'
             sub_record = record_data[key]
-            association = collection.datasource.get_collection(schema[:foreign_collection])
+            association = collection.datasource.get_collection(schema.foreign_collection)
             RecordValidator.validate(association, sub_record)
           else
-            raise ForestException, "Unexpected schema type '#{schema[:type]}' while traversing record"
+            raise ForestException, "Unexpected schema type '#{schema.type}' while traversing record"
           end
         end
       end
