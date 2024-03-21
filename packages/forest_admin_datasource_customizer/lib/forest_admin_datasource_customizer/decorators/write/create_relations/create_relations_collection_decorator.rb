028e76177d608236ff48a5e2b916053fd8319e36
Matt <matthv@gmail.com>
2024-03-21 15:49:29 +0100

    feat(write): set create & update relations decorator

--- packages/forest_admin_datasource_customizer/lib/forest_admin_datasource_customizer/decorators/write/create_relations/create_relations_collection_decorator.rb
+++ packages/forest_admin_datasource_customizer/lib/forest_admin_datasource_customizer/decorators/write/create_relations/create_relations_collection_decorator.rb
@@ -3,6 +3,105 @@ module ForestAdminDatasourceCustomizer
     module Write
       module CreateRelations
         class CreateRelationsCollectionDecorator < ForestAdminDatasourceToolkit::Decorators::CollectionDecorator
+          include ForestAdminDatasourceToolkit::Components::Query
+          include ForestAdminDatasourceToolkit::Components::Query::ConditionTree::Nodes
+          def create(caller, data)
+            # Step 1: Remove all relations from records, and store them in a map
+            # Note: the extractRelations method modifies the records array in place!
+            records_by_relation = extract_relations(data)
+
+            # Step 2: Create the many-to-one relations, and put the foreign keys in the records
+            records_by_relation.each do |key, entries|
+              create_many_to_one_relation(caller, data, key, entries) if schema.fields[key].type == 'ManyToOne'
+            end
+
+            # Step 3: Create the records
+            records_with_pk = child_collection.create(caller, data)
+
+            # Step 4: Create the one-to-one relations
+            # Note: the create_one_to_one_relation method modifies the records_with_pk array in place!
+            records_by_relation.each do |key, entries|
+              create_one_to_one_relation(caller, records_with_pk, key, entries) if schema.fields[key].type == 'OneToOne'
+            end
+
+            records_with_pk
+          end
+
+          private
+
+          def extract_relations(records)
+            records_by_relation = {}
+
+            records.each_with_index do |record, index|
+              next if schema.fields[index].type != 'Column'
+
+              record.each do |key, sub_record|
+                records_by_relation[index] ||= {}
+                records_by_relation[index][key] = sub_record
+              end
+              records.delete_at(index)
+            end
+
+            records_by_relation
+          end
+
+          def create_many_to_one_relation(caller, records, key, entries)
+            schema = schema[:fields][key]
+            relation = datasource.get_collection(schema.foreign_collection)
+            creations = entries.filter { |index| !records[index][schema.foreign_key] }
+            updates = entries.filter { |index| records[index][schema.foreign_key] }
+
+            #         if (! in_array($schema->getForeignKey(), array_keys($records), true)) {
+            #             $relatedRecord = $relation->childCollection->create($caller, $entries);
+            #             $records[$schema->getForeignKey()] = $relatedRecord[$schema->getForeignKeyTarget()];
+            #         }
+
+            # Create the relations when the fk is not present
+            if creations.length.positive?
+              # Not sure which behavior is better (we'll go with the first option for now):
+              # - create a new record for each record in the original create request
+              # - use object-hash to create a single record for each unique subRecord
+              sub_records = # const subRecords = creations.map(({ subRecord }) => subRecord);
+                creations.map do |index|
+                  records[index][key]
+                end
+              related_records = relation.create(caller, sub_records)
+
+              creations.each do |index|
+                records[index][schema.foreign_key] = related_records[index][schema.foreign_key_target]
+              end
+            end
+
+            #      } else {
+            #          $value = $records[$schema->getForeignKey()];
+            #          $conditionTree = new ConditionTreeLeaf($schema->getForeignKeyTarget(), Operators::EQUAL, $value);
+            #          $relation->childCollection->update($caller, new Filter($conditionTree), $entries);
+            #      }
+            #  }
+
+            # Update the relations when the fk is present
+            updates.each do |index, sub_record|
+              value = records[index][schema.foreign_key]
+              condition_tree = ConditionTreeLeaf.new(schema.foreign_key_target, 'Equal', value)
+
+              relation.update(caller, Filter.new(condition_tree: condition_tree), sub_record)
+            end
+          end
+
+          def create_one_to_one_relation(caller, records, key, entries)
+            schema = schema[:fields][key]
+            relation = datasource.get_collection(schema.foreign_collection)
+
+            # Set origin key in the related record
+            sub_records = entries.map do |index, sub_record|
+              sub_record[schema.origin_key] = records[index][schema.origin_key_target]
+            end
+            # sub_records = entries.map
+            # { |index, sub_record| sub_record.merge(schema.origin_key => records[index][schema.origin_key_target]) }
+            # KEY ORIGIN_KEY_TARGET ????
+
+            relation.create(caller, sub_records)
+          end
         end
       end
     end
--- packages/forest_admin_datasource_customizer/lib/forest_admin_datasource_customizer/decorators/write/update_relations/update_relations_collection_decorator.rb
+++ packages/forest_admin_datasource_customizer/lib/forest_admin_datasource_customizer/decorators/write/update_relations/update_relations_collection_decorator.rb
@@ -3,6 +3,90 @@ module ForestAdminDatasourceCustomizer
     module Write
       module UpdateRelations
         class UpdateRelationsCollectionDecorator < ForestAdminDatasourceToolkit::Decorators::CollectionDecorator
+          include ForestAdminDatasourceToolkit::Components::Query
+
+          def update(caller, filter, patch)
+            # Step 1: Perform the normal update
+            if patch.keys.any? { |key| schema[:fields][key].type == 'Column' }
+              patch_without_relations = patch.keys.reduce do |memo, key|
+                schema[:fields][key].type == 'Column' ? memo.merge(key => patch[key]) : memo
+              end
+
+              child_collection.update(caller, filter, patch_without_relations)
+            end
+
+            # Step 2: Perform additional updates for relations
+            return unless patch.keys.any? { |key| schema[:fields][key].type != 'Column' }
+
+            # Fetch the records that will be updated, to know which relations need to be created/updated
+            projection = build_projection(patch)
+            records = list(caller, filter, projection)
+
+            # Perform the updates for each relation
+            patch.keys
+                 .filter { |key| schema[:fields][key].type != 'Column' }
+                 .map { |key| create_or_update_relation(caller, records, key, patch[key]) }
+          end
+
+          private
+
+          # Build a projection that has enough information to know
+          # - which relations need to be created/updated
+          # - the values that will be used to build filters to target records
+          # - the values that will be used to create/update the relations
+          def build_projection(patch)
+            projection = Projection.new.with_pks(self)
+
+            patch.each_key do |key|
+              schema = schema[:fields][key]
+
+              next unless schema.type != 'Column'
+
+              relation = datasource.get_collection(schema.foreign_collection)
+
+              projection = projection.union(Projection.new.with_pks(relation).nest(prefix: key))
+              if schema.type == 'ManyToOne'
+                projection = projection.union(Projection.new(schema.foreign_key_target).nest(prefix: key))
+              end
+              if schema.type == 'OneToOne'
+                projection = projection.union(Projection.new(schema.origin_key_target).nest(prefix: key))
+              end
+            end
+
+            projection
+          end
+
+          def create_or_update_relation(caller, records, key, patch)
+            schema = schema[:fields][key]
+            relation = datasource.get_collection(schema.foreign_collection)
+            creates = records.filter { |r| !r[key] || r[key].nil? }
+            updates = records.filter { |r| r[key] && !r[key].nil? }
+
+            if creates.empty?
+              # Create the one-to-one relations that don't already exist
+              relation.create(
+                caller,
+                creates.map { |record| patch.merge(schema.origin_key => record[schema.origin_key_target]) }
+              )
+            else
+              # Create many-to-one relations
+              sub_record = relation.create(caller, [patch])
+
+              # Set foreign key on the parent records
+              condition_tree = ConditionTree::ConditionTreeFactory.match_records(self.schema, creates)
+              parent_patch = { schema.foreign_key => sub_record[schema.foreign_key_target] }
+
+              update(caller, Filter.new(condition_tree: condition_tree), parent_patch)
+            end
+
+            # Update the relations that already exist
+            return if updates.empty?
+
+            sub_records = updates.map { |record| record[key] }
+            condition_tree = ConditionTree::ConditionTreeFactory.match_records(relation.schema, sub_records)
+
+            relation.update(caller, Filter.new(condition_tree: condition_tree), patch)
+          end
         end
       end
     end
