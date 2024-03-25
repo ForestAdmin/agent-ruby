a178206d93a34ba38ee559b6f800453b8364f1cb
Matt <matthv@gmail.com>
2024-03-25 18:00:32 +0100

    fix: create_relations

--- packages/forest_admin_datasource_customizer/lib/forest_admin_datasource_customizer/decorators/write/create_relations/create_relations_collection_decorator.rb
+++ packages/forest_admin_datasource_customizer/lib/forest_admin_datasource_customizer/decorators/write/create_relations/create_relations_collection_decorator.rb
@@ -12,7 +12,7 @@ module ForestAdminDatasourceCustomizer
 
             # Step 2: Create the many-to-one relations, and put the foreign keys in the records
             records_by_relation.each do |key, entries|
-              create_many_to_one_relation(caller, data, key, entries) if schema.fields[key].type == 'ManyToOne'
+              create_many_to_one_relation(caller, data, key, entries) if schema[:fields][key].type == 'ManyToOne'
             end
 
             # Step 3: Create the records
@@ -21,7 +21,10 @@ module ForestAdminDatasourceCustomizer
             # Step 4: Create the one-to-one relations
             # Note: the create_one_to_one_relation method modifies the records_with_pk array in place!
             records_by_relation.each do |key, entries|
-              create_one_to_one_relation(caller, records_with_pk, key, entries) if schema.fields[key].type == 'OneToOne'
+              if schema[:fields][key].type == 'OneToOne'
+                create_one_to_one_relation(caller, records_with_pk, key,
+                                           entries)
+              end
             end
 
             records_with_pk
@@ -29,78 +32,57 @@ module ForestAdminDatasourceCustomizer
 
           private
 
-          def extract_relations(records)
+          # private function extractRelations(array &$records): array
+          #     {
+          #         $recordsByRelation = [];
+          #
+          #         foreach ($records as $index => $record) {
+          #             if ($this->getFields()[$index]->getType() !== 'Column') {
+          #                 foreach ($record as $key => $subRecord) {
+          #                     $recordsByRelation[$index] ??= [];
+          #                     $recordsByRelation[$index][$key] = $subRecord;
+          #                 }
+          #                 unset($records[$index]);
+          #             }
+          #         }
+          #
+          #         return $recordsByRelation;
+          #     }
+          def extract_relations(record)
             records_by_relation = {}
 
-            records.each_with_index do |record, index|
-              next if schema.fields[index].type != 'Column'
+            record.each do |key, value|
+              next unless schema[:fields][key].type != 'Column'
 
-              record.each do |key, sub_record|
-                records_by_relation[index] ||= {}
-                records_by_relation[index][key] = sub_record
+              value.each do |sub_key, sub_record|
+                records_by_relation[key] ||= {}
+                records_by_relation[key][sub_key] = sub_record
               end
-              records.delete_at(index)
+              record.delete(key)
             end
 
             records_by_relation
           end
 
           def create_many_to_one_relation(caller, records, key, entries)
-            schema = schema[:fields][key]
-            relation = datasource.get_collection(schema.foreign_collection)
-            creations = entries.filter { |index| !records[index][schema.foreign_key] }
-            updates = entries.filter { |index| records[index][schema.foreign_key] }
+            field_schema = schema[:fields][key]
+            relation = datasource.get_collection(field_schema.foreign_collection)
 
-            #         if (! in_array($schema->getForeignKey(), array_keys($records), true)) {
-            #             $relatedRecord = $relation->childCollection->create($caller, $entries);
-            #             $records[$schema->getForeignKey()] = $relatedRecord[$schema->getForeignKeyTarget()];
-            #         }
-
-            # Create the relations when the fk is not present
-            if creations.length.positive?
-              # Not sure which behavior is better (we'll go with the first option for now):
-              # - create a new record for each record in the original create request
-              # - use object-hash to create a single record for each unique subRecord
-              sub_records = # const subRecords = creations.map(({ subRecord }) => subRecord);
-                creations.map do |index|
-                  records[index][key]
-                end
-              related_records = relation.create(caller, sub_records)
-
-              creations.each do |index|
-                records[index][schema.foreign_key] = related_records[index][schema.foreign_key_target]
-              end
-            end
-
-            #      } else {
-            #          $value = $records[$schema->getForeignKey()];
-            #          $conditionTree = new ConditionTreeLeaf($schema->getForeignKeyTarget(), Operators::EQUAL, $value);
-            #          $relation->childCollection->update($caller, new Filter($conditionTree), $entries);
-            #      }
-            #  }
-
-            # Update the relations when the fk is present
-            updates.each do |index, sub_record|
-              value = records[index][schema.foreign_key]
-              condition_tree = ConditionTreeLeaf.new(schema.foreign_key_target, 'Equal', value)
-
-              relation.update(caller, Filter.new(condition_tree: condition_tree), sub_record)
+            if records.key?(field_schema.foreign_key)
+              value = records[field_schema.foreign_key]
+              condition_tree = ConditionTreeLeaf.new(field_schema.foreign_key_target, Operators::EQUAL, value)
+              relation.update(caller, Filter.new(condition_tree: condition_tree), entries)
+            else
+              related_record = relation.create(caller, entries)
+              records[field_schema.foreign_key] = related_record[field_schema.foreign_key_target]
             end
           end
 
           def create_one_to_one_relation(caller, records, key, entries)
-            schema = schema[:fields][key]
-            relation = datasource.get_collection(schema.foreign_collection)
+            field_schema = schema[:fields][key]
+            relation = datasource.get_collection(field_schema.foreign_collection)
 
-            # Set origin key in the related record
-            sub_records = entries.map do |index, sub_record|
-              sub_record[schema.origin_key] = records[index][schema.origin_key_target]
-            end
-            # sub_records = entries.map
-            # { |index, sub_record| sub_record.merge(schema.origin_key => records[index][schema.origin_key_target]) }
-            # KEY ORIGIN_KEY_TARGET ????
-
-            relation.create(caller, sub_records)
+            relation.create(caller, entries.merge(field_schema.origin_key_target => records[field_schema.origin_key]))
           end
         end
       end
