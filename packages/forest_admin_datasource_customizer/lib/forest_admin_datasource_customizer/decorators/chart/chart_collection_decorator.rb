bb3c20c135c90282454f8628440db7a9c4494acd
Nicolas Alexandre <nicolasalexandre9@gmail.com>
2024-03-28 11:28:47 +0100

    feat(chart): add collection decorator

--- packages/forest_admin_datasource_customizer/lib/forest_admin_datasource_customizer/decorators/chart/chart_collection_decorator.rb
+++ packages/forest_admin_datasource_customizer/lib/forest_admin_datasource_customizer/decorators/chart/chart_collection_decorator.rb
@@ -4,6 +4,35 @@ module ForestAdminDatasourceCustomizer
       class ChartCollectionDecorator < ForestAdminDatasourceToolkit::Decorators::CollectionDecorator
         include ForestAdminDatasourceToolkit::Decorators
         include ForestAdminDatasourceToolkit::Components::Query::ConditionTree
+
+        def initialize(child_collection, datasource)
+          @charts = {}
+          super
+        end
+
+        def add_chart(name, &definition)
+          raise(Exceptions::ForestException, "Chart #{name} already exists.") if schema[:charts].include?(name)
+
+          @charts[name] = definition
+          mark_schema_as_dirty
+        end
+
+        def render_chart(caller, name, record_id)
+          if @charts.key?(name)
+            context = ChartContext.new(self, caller, record_id)
+            result_builder = ResultBuilder.new
+
+            return @charts[name].call(context, result_builder)
+          end
+
+          @child_collection.render_chart(caller, name, record_id)
+        end
+
+        def refine_schema(sub_schema)
+          sub_schema[:charts] = sub_schema[:charts] + @charts.keys
+
+          sub_schema
+        end
       end
     end
   end
