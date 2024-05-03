c912a341640f8374c7d0ea9f15d7f396d47f85ce
Nicolas Alexandre <nicolasalexandre9@gmail.com>
2024-05-03 11:49:16 +0200

    feat: add prefix config

--- packages/forest_admin_rails/config/routes.rb
+++ packages/forest_admin_rails/config/routes.rb
@@ -1,8 +1,12 @@
 ForestAdminRails::Engine.routes.draw do
   Rails.error.handle(ForestAdminDatasourceToolkit::Exceptions::ForestException) do
     ForestAdminAgent::Http::Router.routes.each do |name, agent_route|
-      match agent_route[:uri], defaults: { format: agent_route[:format] }, to: 'forest#index',
-                               via: agent_route[:method], as: name, route_alias: name
+      match "#{ForestAdminAgent::Facades::Container.cache(:prefix)}/#{agent_route[:uri]}",
+            defaults: { format: agent_route[:format] },
+            to: 'forest#index',
+            via: agent_route[:method],
+            as: name,
+            route_alias: name
     end
   end
 end
