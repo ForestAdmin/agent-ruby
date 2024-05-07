e1179b5aa9b8322bda69a5e2ed0f8d2df5db919a
Matt <matthv@gmail.com>
2024-05-07 16:08:58 +0200

    feat(override): add update & delete behavior

--- packages/forest_admin_datasource_customizer/lib/forest_admin_datasource_customizer/decorators/override/override_collection_decorator.rb
+++ packages/forest_admin_datasource_customizer/lib/forest_admin_datasource_customizer/decorators/override/override_collection_decorator.rb
@@ -5,17 +5,10 @@ module ForestAdminDatasourceCustomizer
         include Context
         attr_reader :create_handler, :update_handler, :delete_handler
 
-        def initialize(child_collection, datasource)
-          super
-          @create_handler = Handler.new
-          @update_handler = Handler.new
-          @delete_handler = Handler.new
-        end
-
         def create(caller, data)
           if @create_handler
             context = CreateOverrideCustomizationContext.new(@child_collection, caller, data)
-            return @create_handler.execute(context)
+            return @create_handler.call(context)
           end
 
           super
@@ -23,6 +16,32 @@ module ForestAdminDatasourceCustomizer
 
         def add_create_handler(handler)
           @create_handler = handler
+        end
+
+        def update(caller, filter, patch)
+          if @update_handler
+            context = UpdateOverrideCustomizationContext.new(@child_collection, caller, filter, patch)
+            return @update_handler.call(context)
+          end
+
+          super
+        end
+
+        def add_update_handler(handler)
+          @update_handler = handler
+        end
+
+        def delete(caller, filter)
+          if @delete_handler
+            context = DeleteOverrideCustomizationContext.new(@child_collection, caller, filter)
+            return @delete_handler.call(context)
+          end
+
+          super
+        end
+
+        def add_delete_handler(handler)
+          @delete_handler = handler
         end
       end
     end
