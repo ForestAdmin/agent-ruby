cdf08e5ee63f94dced26c0291d3c1e3ebc5f2b28
Nicolas Alexandre <nicolasalexandre9@gmail.com>
2024-06-11 09:37:56 +0200

    feat: add new polymorphic relation schema

---
+++ packages/forest_admin_datasource_toolkit/lib/forest_admin_datasource_toolkit/schema/relations/polymorphic_many_to_one_schema.rb
@@ -0,0 +1,24 @@
+module ForestAdminDatasourceToolkit
+  module Schema
+    module Relations
+      class PolymorphicManyToOneSchema
+        # attr_accessor :foreign_key
+        attr_reader :foreign_key_target, :foreign_key, :foreign_key_targets, :foreign_key_type_field,
+                    :foreign_collections, :type
+
+        def initialize(
+          foreign_key_type_field:,
+          foreign_key:,
+          foreign_key_targets:,
+          foreign_collections:
+        )
+          @foreign_key = foreign_key
+          @foreign_key_targets = foreign_key_targets
+          @foreign_key_type_field = foreign_key_type_field
+          @foreign_collections = foreign_collections
+          @type = 'PolymorphicManyToOne'
+        end
+      end
+    end
+  end
+end
