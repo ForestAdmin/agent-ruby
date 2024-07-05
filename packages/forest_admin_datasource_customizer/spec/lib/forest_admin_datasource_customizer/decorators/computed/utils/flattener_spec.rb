73384af319dcbad96b77a5f2916bc15b03a62076
Nicolas Alexandre <nicolasalexandre9@gmail.com>
2024-07-05 15:43:41 +0200

    test(comuted): update tests on flattener

--- packages/forest_admin_datasource_customizer/spec/lib/forest_admin_datasource_customizer/decorators/computed/utils/flattener_spec.rb
+++ packages/forest_admin_datasource_customizer/spec/lib/forest_admin_datasource_customizer/decorators/computed/utils/flattener_spec.rb
@@ -9,9 +9,9 @@ module ForestAdminDatasourceCustomizer
           it 'unflatten() should work with simple case' do
             flat_list = [
               [1, 2, 3],
-              ['romain', nil, 'ana']
+              ['romain', Flattener::Undefined.new, 'ana']
             ]
-            projection = Projection.new(['id', 'book:author:firstname'])
+            projection = Projection.new(%w[id book:author:firstname])
 
             expect(described_class.un_flatten(flat_list, projection)).to eq(
               [
@@ -22,12 +22,12 @@ module ForestAdminDatasourceCustomizer
             )
           end
 
-          it 'unflatten() should work with multiple null' do
+          it 'unflatten() should work with multiple undefined' do
             flat_list = [
-              [nil],
+              [Flattener::Undefined.new],
               [15],
               [26],
-              [nil]
+              [Flattener::Undefined.new]
             ]
             projection = Projection.new(
               %w[rental:customer:name rental:id rental:numberOfDays rental:customer:id]
@@ -40,20 +40,67 @@ module ForestAdminDatasourceCustomizer
             )
           end
 
-          it 'flatten() should work' do
-            records = [
-              { 'id' => 1, 'book' => { 'author' => { 'firstname' => 'romain' } } },
-              { 'id' => 2, 'book' => nil },
-              { 'id' => 3, 'book' => { 'author' => { 'firstname' => 'ana' } } }
-            ]
-            projection = Projection.new(['id', 'book:author:firstname'])
-
-            expect(described_class.flatten(records, projection)).to eq(
-              [
-                [1, 2, 3],
-                ['romain', nil, 'ana']
+          describe 'flatten' do
+            it 'works with simple case' do
+              records = [
+                { 'id' => 1, 'book' => { 'author' => { 'firstname' => 'romain' } } },
+                { 'id' => 2, 'book' => nil },
+                { 'id' => 3, 'book' => { 'author' => { 'firstname' => 'ana' } } }
               ]
-            )
+              projection = Projection.new(%w[id book:author:firstname])
+
+              expect(described_class.flatten(records, projection)).to contain_exactly(
+                contain_exactly(1, 2, 3),
+                contain_exactly('romain', an_instance_of(Flattener::Undefined), 'ana')
+              )
+            end
+
+            it 'round trip with markers should conserve null values' do
+              records = [
+                { 'id' => 1 },
+                { 'id' => 2, 'book' => nil },
+                { 'id' => 3, 'book' => { 'author' => nil } },
+                { 'id' => 4, 'book' => { 'author' => { 'firstname' => 'Isaac', 'lastname' => 'Asimov' } } }
+              ]
+              projection = Projection.new(%w[id book:author:firstname book:author:lastname])
+              projection_with_marker = described_class.with_null_marker(projection)
+              flattened = described_class.flatten(records, projection_with_marker)
+              unflattened = described_class.un_flatten(flattened, projection_with_marker)
+
+              expect(projection_with_marker).to eq(
+                %w[id book:author:firstname book:author:lastname book:__null_marker book:author:__null_marker]
+              )
+
+              expect(flattened).to contain_exactly(
+                contain_exactly(1, 2, 3, 4),
+                contain_exactly(
+                  an_instance_of(Flattener::Undefined),
+                  an_instance_of(Flattener::Undefined),
+                  an_instance_of(Flattener::Undefined),
+                  'Isaac'
+                ),
+                contain_exactly(
+                  an_instance_of(Flattener::Undefined),
+                  an_instance_of(Flattener::Undefined),
+                  an_instance_of(Flattener::Undefined),
+                  'Asimov'
+                ),
+                contain_exactly(
+                  an_instance_of(Flattener::Undefined),
+                  nil,
+                  an_instance_of(Flattener::Undefined),
+                  an_instance_of(Flattener::Undefined)
+                ),
+                contain_exactly(
+                  an_instance_of(Flattener::Undefined),
+                  an_instance_of(Flattener::Undefined),
+                  nil,
+                  an_instance_of(Flattener::Undefined)
+                )
+              )
+
+              expect(unflattened).to eq(records)
+            end
           end
         end
       end
