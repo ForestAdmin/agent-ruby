55370d82e907f0c52f85a284d94905f303841bdd
Nicolas Alexandre <nicolasalexandre9@gmail.com>
2024-08-02 11:43:01 +0200

    test: update test on datasource active record

--- packages/forest_admin_datasource_active_record/spec/lib/forest_admin_datasource_active_record/datasource_spec.rb
+++ packages/forest_admin_datasource_active_record/spec/lib/forest_admin_datasource_active_record/datasource_spec.rb
@@ -4,7 +4,7 @@ module ForestAdminDatasourceActiveRecord
   describe Datasource do
     it 'fetch all models' do
       datasource = described_class.new(adapter: 'sqlite3', database: 'db/database.db')
-      expected = %w[user order check category car_check car address]
+      expected = %w[User Order Check Category CarCheck Car Address]
 
       expect(datasource.collections.keys).to match_array(expected)
     end
