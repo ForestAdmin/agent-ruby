d1be7475892a69179ca91416c9050d79cde1beff
Nicolas Alexandre <nicolasalexandre9@gmail.com>
2024-08-05 17:41:12 +0200

    fix: delete polymorphic target reset the polymorphic record

--- packages/forest_admin_datasource_active_record/lib/forest_admin_datasource_active_record/collection.rb
+++ packages/forest_admin_datasource_active_record/lib/forest_admin_datasource_active_record/collection.rb
@@ -35,7 +35,7 @@ module ForestAdminDatasourceActiveRecord
 
     def update(_caller, filter, data)
       entity = Utils::Query.new(self, nil, filter).build.first
-      entity.update(data)
+      entity&.update(data)
     end
 
     def delete(_caller, filter)
