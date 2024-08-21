92ada7c356d99f41f6b2e8c8d80e576c80664315
Nicolas Alexandre <nicolasalexandre9@gmail.com>
2024-08-21 11:19:02 +0200

    fix: rescue error when get the klass of association

--- packages/forest_admin_datasource_active_record/lib/forest_admin_datasource_active_record/parser/relation.rb
+++ packages/forest_admin_datasource_active_record/lib/forest_admin_datasource_active_record/parser/relation.rb
@@ -3,10 +3,11 @@ module ForestAdminDatasourceActiveRecord
     module Relation
       def associations(model, support_polymorphic_relations: false)
         model.reflect_on_all_associations.select do |association|
+          is_valid_association = !get_class(association).nil? && !active_type?(get_class(association))
           if support_polymorphic_relations
-            polymorphic?(association) ? true : !active_type?(association.klass)
+            polymorphic?(association) ? true : is_valid_association
           else
-            !polymorphic?(association) && !active_type?(association.klass)
+            !polymorphic?(association) && is_valid_association
           end
         end
       end
@@ -14,6 +15,12 @@ module ForestAdminDatasourceActiveRecord
       def polymorphic?(association)
         (association.options.key?(:polymorphic) && association.options[:polymorphic]) ||
           association.inverse_of&.polymorphic?
+      end
+
+      def get_class(association)
+        association.klass
+      rescue StandardError
+        nil
       end
 
       # NOTICE: Ignores ActiveType::Object association during introspection and interactions.
