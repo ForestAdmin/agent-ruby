3223a37bf032d6c96bcdbee81bf8171c64bb8dd9
Matt <matthv@gmail.com>
2024-09-23 14:59:15 +0200

    feat(form): add new RowElement to LayoutElement

--- packages/forest_admin_datasource_customizer/lib/forest_admin_datasource_customizer/decorators/action/form_layout_element.rb
+++ packages/forest_admin_datasource_customizer/lib/forest_admin_datasource_customizer/decorators/action/form_layout_element.rb
@@ -3,13 +3,13 @@ module ForestAdminDatasourceCustomizer
     module Action
       module FormLayoutElement
         include Types
+        include ForestAdminDatasourceToolkit::Exceptions
 
         class LayoutElement < BaseFormElement
           attr_accessor :if_condition, :component
 
           def initialize(component:, if_condition: nil, **kwargs)
             super(type: 'Layout', **kwargs)
-
             @component = component
             @if_condition = if_condition
           end
@@ -27,6 +27,38 @@ module ForestAdminDatasourceCustomizer
           def initialize(content:, **options)
             super(component: 'HtmlBlock', **options)
             @content = content
+          end
+        end
+
+        class RowElement < LayoutElement
+          attr_accessor :fields
+
+          def initialize(options)
+            super(component: 'Row', **options)
+            validate_fields_presence!(options)
+            validate_no_layout_subfields!(options[:fields])
+            @fields = instantiate_subfields(options[:fields] || [])
+          end
+
+          private
+
+          def validate_fields_presence!(options)
+            raise ForestException, "Using 'fields' in a 'Row' configuration is mandatory" unless options.key?(:fields)
+          end
+
+          def validate_no_layout_subfields!(fields)
+            fields.each do |field|
+              if (field.is_a?(DynamicField) && field.type == 'Layout') ||
+                 (field.is_a?(Hash) && field[:type] == 'Layout')
+                raise ForestException, "A 'Row' form element doesn't allow layout elements as subfields"
+              end
+            end
+          end
+
+          def instantiate_subfields(fields)
+            fields.map do |field|
+              ForestAdminDatasourceToolkit::Components::Actions::ActionFieldFactory.build(field.to_h)
+            end
           end
         end
       end
