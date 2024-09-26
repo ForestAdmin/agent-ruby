b0ff2c8b6560d215525f24320d86c838a2d483be
Matt <matthv@gmail.com>
2024-09-26 18:13:13 +0200

    fix: action layout

--- packages/forest_admin_datasource_toolkit/lib/forest_admin_datasource_toolkit/components/actions/action_layout_element.rb
+++ packages/forest_admin_datasource_toolkit/lib/forest_admin_datasource_toolkit/components/actions/action_layout_element.rb
@@ -49,19 +49,31 @@ module ForestAdminDatasourceToolkit
 
           def initialize(fields:, **options)
             super(component: 'Row', **options)
-            @fields = fields
+            @fields = instantiate_subfields(fields)
+          end
+
+          def instantiate_subfields(fields)
+            fields.map do |field|
+              ActionField.new(**field.to_h)
+            end
           end
         end
 
         class PageElement < BaseLayoutElement
           attr_accessor :elements, :next_button_label, :previous_button_label
 
-          def initialize(elements:, **options)
+          def initialize(elements:, previous_button_label:, next_button_label:, **options)
             super(component: 'Page', **options)
             @elements = elements
             @next_button_label = next_button_label
             @previous_button_label = previous_button_label
-            @elements = elements
+            @elements = instantiate_elements(elements)
+          end
+
+          def instantiate_elements(elements)
+            elements.map do |element|
+              ActionFieldFactory.build(**element.to_h)
+            end
           end
         end
       end
