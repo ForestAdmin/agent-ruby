d04b7e3e593a28ca7eb1c2a32fd08ecc81261d5d
Matt <matthv@gmail.com>
2024-10-15 17:28:13 +0200

    feat(capabilites): add new route

---
+++ packages/forest_admin_agent/lib/forest_admin_agent/routes/capabilities/collections.rb
@@ -0,0 +1,46 @@
+require 'json'
+module ForestAdminAgent
+  module Routes
+    module Capabilities
+      class Collections < AbstractRoute
+        include ForestAdminDatasourceToolkit::Schema
+
+        def setup_routes
+          add_route('forest_capabilities_collections', 'post', '/capabilities', ->(args) { handle_request(args) })
+
+          self
+        end
+
+        def handle_request(args = {})
+          @datasource = ForestAdminAgent::Facades::Container.datasource
+          collections = args[:params]['collectionNames'] || @datasource.collections.keys
+
+          result = collections.map do |collection_name|
+            collection = @datasource.get_collection(collection_name)
+            {
+              collectionName: collection.name,
+              fields: collection.schema[:fields].select { |_, field| field.is_a?(ColumnSchema) }.map do |name, field|
+                {
+                  fieldName: name,
+                  fieldType: field.column_type,
+                  operators: field.filter_operators.map { |operator| to_pascal_case(operator) }
+                }
+              end
+            }
+          end
+
+          {
+            content: result,
+            status: 200
+          }
+        end
+
+        private
+
+        def to_pascal_case(string)
+          string.split('_').map(&:capitalize).join
+        end
+      end
+    end
+  end
+end
