b2b976ecf24a8c1d808ec3ce67e2a69f563a24c6
Matt <matthv@gmail.com>
2025-01-22 16:07:15 +0100

    chore: remove habtm logic

--- packages/forest_admin_datasource_mongoid/lib/forest_admin_datasource_mongoid/datasource.rb
+++ packages/forest_admin_datasource_mongoid/lib/forest_admin_datasource_mongoid/datasource.rb
@@ -10,12 +10,6 @@ module ForestAdminDatasourceMongoid
       generate
     end
 
-    def simulate_habtm(model)
-      model.reflect_on_all_associations(:has_and_belongs_to_many).each do |association|
-        make_through_model(model, association) unless collections.key?(create_through_model_name(model, association))
-      end
-    end
-
     private
 
     def generate
@@ -32,55 +26,14 @@ module ForestAdminDatasourceMongoid
       klass.relations.any? { |_name, association| association.is_a?(Mongoid::Association::Embedded::EmbeddedIn) }
     end
 
-    def make_through_model(model, association)
-      # make a fake model to represent the through model
-      Class.new do
-        include Mongoid::Document
-        class << self
-          attr_accessor :name, :collection_name, :primary_key
-        end
-
-        def self.add_association(name, options)
-          belongs_to name, required: false, **options
-        end
+    def fetch_primary_key(klass)
+      primary_key = klass.fields.find { |_, field| field.options[:identity] || field.name == '_id' }&.first
+      unless primary_key
+        raise(ForestAdminDatasourceToolkit::Exceptions::ForestException,
+              "Primary key not found for #{klass.name}")
       end
 
-      options = {}
-      options[:name] = create_through_model_name(model, association)
-      options[:collection_name] = options[:name].downcase
-
-      options[:associations] = [
-        {
-          name: model.name.demodulize.underscore.to_sym,
-          foreign_collection: model.name.gsub('::', '__'),
-          foreign_key: fetch_foreign_key(model),
-          foreign_key_target: fetch_primary_key(association.klass)
-        },
-        {
-          name: association.klass.name.demodulize.underscore.to_sym,
-          foreign_collection: association.klass.name.gsub('::', '__'),
-          foreign_key: fetch_foreign_key(association.klass),
-          foreign_key_target: fetch_primary_key(model)
-        }
-      ]
-
-      add_collection(ThroughCollection.new(self, options))
-    end
-
-    def create_through_model_name(model, association)
-      [model, association.klass]
-        .map { |klass| klass.name.split('::').last }
-        .sort
-        .join('_')
-    end
-
-    def fetch_primary_key(klass)
-      klass.fields.find { |_, field| field.options[:identity] || field.name == '_id' }&.first
-    end
-
-    def fetch_foreign_key(klass)
-      klass.fields.find { |_, field| field.options[:identity] }&.first || '_id'
-      # fetch_primary_key(klass).to_s
+      primary_key
     end
   end
 end
