b38b31e1bd27bbd453a72eb7ed9a88e2e9dc0439
Matt <matthv@gmail.com>
2025-01-22 16:24:45 +0100

    chore: add test for datasource

---
+++ packages/forest_admin_datasource_mongoid/spec/lib/forest_admin_datasource_mongoid/datasource_spec.rb
@@ -0,0 +1,36 @@
+require 'spec_helper'
+
+module ForestAdminDatasourceMongoid
+  include ForestAdminDatasourceToolkit::Exceptions
+  describe Datasource do
+    let(:datasource) { described_class.new }
+
+    it 'adds collections to the datasource' do
+      expected = %w[Band Author Tag Comment Departure Post User Team]
+
+      expect(datasource.collections.keys).to match_array(expected)
+    end
+
+    it 'only loads models that are Mongoid::Document classes' do
+      expect(datasource.collections.keys).not_to include('NotMongoidModel')
+    end
+
+    it 'does not load embedded models as collections' do
+      expect(datasource.collections.keys).not_to include('Address')
+    end
+
+    it 'does not load any models if no Mongoid::Document classes are present' do
+      # Simulates the absence of Mongoid models in the application by forcing the each_object method
+      # to return an empty list. This allows testing the behavior of the Datasource when no Mongoid models are available.
+      allow(ObjectSpace).to receive(:each_object).and_return([])
+
+      datasource = described_class.new
+      expect(datasource.collections.keys).to be_empty
+    end
+
+    it 'raises an exception if trying to add a collection with an existing name' do
+      expect { datasource.add_collection(Collection.new(datasource, Post)) }
+        .to raise_error(ForestException, '🌳🌳🌳 Collection Post already defined in datasource')
+    end
+  end
+end
