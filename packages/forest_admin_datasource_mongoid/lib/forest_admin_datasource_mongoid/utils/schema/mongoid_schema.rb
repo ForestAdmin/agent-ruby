a66a9956eaeb1134f7a7ee65c28a6dbce99f7ec3
Matt <matthv@gmail.com>
2025-02-03 17:22:31 +0100

    feat(flatten): handle list_path_matching

--- packages/forest_admin_datasource_mongoid/lib/forest_admin_datasource_mongoid/utils/schema/mongoid_schema.rb
+++ packages/forest_admin_datasource_mongoid/lib/forest_admin_datasource_mongoid/utils/schema/mongoid_schema.rb
@@ -2,13 +2,21 @@ module ForestAdminDatasourceMongoid
   module Utils
     module Schema
       class MongoidSchema
+        include ForestAdminDatasourceToolkit::Exceptions
         attr_reader :is_array, :is_leaf, :fields
 
         def initialize(model, fields, is_array, is_leaf)
+          @models = ObjectSpace.each_object(Class)
+                               .select { |klass| klass < Mongoid::Document && klass.name && !klass.name.start_with?('Mongoid::') }
+                               .to_h { |klass| [klass.name, klass] }
           @model = model
           @fields = fields
           @is_array = is_array
           @is_leaf = is_leaf
+        end
+
+        def schema_node
+          @is_leaf ? @fields[:content] : @fields
         end
 
         def self.from_model(model)
@@ -55,6 +63,58 @@ module ForestAdminDatasourceMongoid
             target[prefix] ||= {}
             recursive_set(target[prefix], suffix, value)
           end
+        end
+
+        def list_paths_matching(handle, prefix = nil)
+          return [] if @is_leaf
+
+          @fields.keys
+                 .filter(&:present?)
+                 .flat_map do |field|
+            schema = get_sub_schema(field)
+            sub_prefix = prefix ? "#{prefix}.#{field}" : field
+            sub_fields = schema.list_paths_matching(handle, sub_prefix)
+            sub_fields.map { |sub_field| "#{field}.#{sub_field}" }
+            handle.call(sub_prefix, schema) ? [field, *sub_fields] : sub_fields
+          end
+        end
+
+        def get_sub_schema(path)
+          # Terminating condition
+          return self if path.blank?
+
+          # General case: go down the tree
+          prefix, suffix = path.split(/\.(.*)/)
+          is_array = false
+          is_leaf = false
+          child = @fields[prefix]
+
+          # Traverse relations
+          if child.is_a?(Hash)
+            relation_name = @model.relations[prefix].class_name
+
+            # Traverse arrays
+            if child.is_a?(Hash) && child['[]']
+              # (has_many embed)
+              child = child['[]']
+              is_array = true
+            else
+              # (has_one embed)
+              child = MongoidSchema.from_model(@models[relation_name]).fields
+            end
+
+            return MongoidSchema.new(@models[relation_name], child, is_array, is_leaf).get_sub_schema(suffix)
+          elsif child.nil?
+            raise ForestException, "Field '#{prefix}' not found. Available fields are: balbalblablalblalalbla"
+          end
+
+          # We ended up on a field => box it.
+          if child.is_a? Mongoid::Fields::Standard
+            child = { content: child }
+            is_leaf = true
+          end
+
+          MongoidSchema.new(@model, child, is_array, is_leaf).get_sub_schema(suffix)
         end
       end
     end
