247d2960a50dad6f3d4bb6aaeaca9f791d6e2d83
Matt <matthv@gmail.com>
2025-02-14 10:41:58 +0100

    fix(utils): add_null_values

--- packages/forest_admin_datasource_mongoid/lib/forest_admin_datasource_mongoid/utils/add_null_values.rb
+++ packages/forest_admin_datasource_mongoid/lib/forest_admin_datasource_mongoid/utils/add_null_values.rb
@@ -4,17 +4,22 @@ module ForestAdminDatasourceMongoid
       # Filter out records that have been tagged as not existing
       # If the key FOREST_RECORD_DOES_NOT_EXIST is present in the record, the record is removed
       # If a nested object has a key with FOREST_RECORD_DOES_NOT_EXIST, the nested object is removed
+      # rubocop:disable Style/ConditionalAssignment
       def remove_not_exist_record(record)
         return nil if record.nil? || record[Pipeline::ConditionGenerator::FOREST_RECORD_DOES_NOT_EXIST]
 
         record.each do |key, value|
           if value.is_a?(Hash) && value.value?(Pipeline::ConditionGenerator::FOREST_RECORD_DOES_NOT_EXIST)
             record[key] = nil
+          else
+            # TODO : TEST PROPERLY - Récursion pour les sous-objets
+            record[key] = remove_not_exist_record(value)
           end
         end
 
         record
       end
+      # rubocop:enable Style/ConditionalAssignment
 
       def add_null_values_on_record(record, projection)
         return nil if record.nil?
@@ -29,7 +34,7 @@ module ForestAdminDatasourceMongoid
         nested_prefixes = projection.select { |field| field.include?(':') }.map { |field| field.split(':').first }.uniq
 
         nested_prefixes.each do |nested_prefix|
-          child_paths = projection.fitler { |field| field.start_with?("#{nested_prefix}:") }
+          child_paths = projection.filter { |field| field.start_with?("#{nested_prefix}:") }
                                   .map { |field| field[nested_prefix.size + 1..] }
 
           next unless result[nested_prefix] && !result[nested_prefix].nil?
