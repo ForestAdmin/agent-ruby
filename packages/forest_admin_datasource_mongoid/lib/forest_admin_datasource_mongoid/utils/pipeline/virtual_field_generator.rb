1215bc070a6675e54a56c2c3510c32a63182d19a
Nicolas Alexandre <nicolasalexandre9@gmail.com>
2025-02-19 09:50:10 +0100

    feat(mongo): add virtual field generatoe

---
+++ packages/forest_admin_datasource_mongoid/lib/forest_admin_datasource_mongoid/utils/pipeline/virtual_field_generator.rb
@@ -0,0 +1,75 @@
+module ForestAdminDatasourceMongoid
+  module Utils
+    module Pipeline
+      # When using the `asModel` options, users can request/filter on the virtual _id and parentId fields
+      # of children (using the generated OneToOne relation).
+      #
+      # As those fields are not written to mongo, they are injected here so that they can be used like
+      # any other field.
+      #
+      # This could be also be done by preprocessing the filter, and postprocessing the records, but this
+      # solution seemed simpler, at the cost of additional pipeline stages when making queries.
+      #
+      # Note that a projection is taken as a parameter so that only fields which are actually used are
+      # injected to save resources.
+      class VirtualFieldGenerator
+        # include Utils::Schema
+
+        def self.add_virtual(_model, stack, projection)
+          set = {}
+
+          projection.each do |colon_field|
+            field = colon_field.tr(':', '.')
+            is_from_one_to_one = stack[stack.length - 1][:as_models].some { |f| field.start_with?("#{f}.") }
+
+            set[field] = get_path(field) if is_from_one_to_one
+          end
+
+          set.keys.length ? [{ '$addFields' => set }] : []
+        end
+
+        def self.get_path(field)
+          id_identifier = '._id'
+          if field.end_with?(id_identifier)
+            suffix = field[0..(field.length - id_identifier.length)]
+
+            return ConditionGenerator.tag_record_if_not_exist_by_value(
+              suffix,
+              { '$concat' => [{ '$toString' => '$_id' }, `.${suffix}`] }
+            )
+          end
+
+          parent_id_identifier = '.parent_id'
+          if field.end_with?(parent_id_identifier)
+            if field.split('.').length > 2
+              # Implementing this would require us to have knowledge of the value of asModel for
+              # for virtual models under the current one, which the `stack` variable does not have.
+
+              # If the expcetion causes issues we could simply return
+              # `$${field.substring(0, field.length - 9)}._id` but that would not work if the customer
+              # jumped over multiple levels of nesting.
+
+              # As this is a use case that never happens from the UI, and that can be worked around when
+              # using the API, we decided to not implement it.
+              raise ForestAdminDatasourceToolkit::Exceptions::ForestException,
+                    'Fetching virtual parentId deeper than 1 level is not supported.'
+            end
+
+            suffix = field[0..(field.length - parent_id_identifier.length)]
+            return ConditionGenerator.tag_record_if_not_exist_by_value(suffix, '$_id')
+          end
+
+          content_identifier = '.content'
+          if field.end_with?(content_identifier)
+            # FIXME: we should check that this is really a leaf field because "content" can't
+            # really be used as a reserved word
+            return "$#{field[0..(field.length - content_identifier.length)]}"
+          end
+
+          parent = field[0..field.rindex('.')]
+          ConditionGenerator.tag_record_if_not_exist_by_value(parent, "$#{field}")
+        end
+      end
+    end
+  end
+end
