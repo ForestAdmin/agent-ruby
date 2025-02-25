f31349a054da16731e13579fb9dd4c71b06ee925
Matt <matthv@gmail.com>
2025-02-25 11:09:56 +0100

    fix: virtual field & add test

--- packages/forest_admin_datasource_mongoid/lib/forest_admin_datasource_mongoid/utils/pipeline/virtual_field_generator.rb
+++ packages/forest_admin_datasource_mongoid/lib/forest_admin_datasource_mongoid/utils/pipeline/virtual_field_generator.rb
@@ -13,34 +13,34 @@ module ForestAdminDatasourceMongoid
       # Note that a projection is taken as a parameter so that only fields which are actually used are
       # injected to save resources.
       class VirtualFieldGenerator
-        # include Utils::Schema
-
         def self.add_virtual(_model, stack, projection)
           set = {}
 
           projection.each do |colon_field|
             field = colon_field.tr(':', '.')
-            is_from_one_to_one = stack[stack.length - 1][:as_models].some { |f| field.start_with?("#{f}.") }
+            is_from_one_to_one = stack.last[:as_models].any? { |f| field.start_with?("#{f}.") }
 
             set[field] = get_path(field) if is_from_one_to_one
           end
 
-          set.keys.length ? [{ '$addFields' => set }] : []
+          set.keys.empty? ? [] : [{ '$addFields' => set }]
         end
 
         def self.get_path(field)
           id_identifier = '._id'
           if field.end_with?(id_identifier)
-            suffix = field[0..(field.length - id_identifier.length)]
+            # ... dots to exclude the last character (ex: 'author.' => 'author')
+            suffix = field[0...(field.length - id_identifier.length)]
 
             return ConditionGenerator.tag_record_if_not_exist_by_value(
               suffix,
-              { '$concat' => [{ '$toString' => '$_id' }, `.${suffix}`] }
+              { '$concat' => [{ '$toString' => '$_id' }, (suffix.empty? ? '' : ".#{suffix}")] }
             )
           end
 
           parent_id_identifier = '.parent_id'
           if field.end_with?(parent_id_identifier)
+
             if field.split('.').length > 2
               # Implementing this would require us to have knowledge of the value of asModel for
               # for virtual models under the current one, which the `stack` variable does not have.
@@ -52,10 +52,10 @@ module ForestAdminDatasourceMongoid
               # As this is a use case that never happens from the UI, and that can be worked around when
               # using the API, we decided to not implement it.
               raise ForestAdminDatasourceToolkit::Exceptions::ForestException,
-                    'Fetching virtual parentId deeper than 1 level is not supported.'
+                    'Fetching virtual parent_id deeper than 1 level is not supported.'
             end
+            suffix = field[0...(field.length - parent_id_identifier.length)]
 
-            suffix = field[0..(field.length - parent_id_identifier.length)]
             return ConditionGenerator.tag_record_if_not_exist_by_value(suffix, '$_id')
           end
 
@@ -63,10 +63,13 @@ module ForestAdminDatasourceMongoid
           if field.end_with?(content_identifier)
             # FIXME: we should check that this is really a leaf field because "content" can't
             # really be used as a reserved word
-            return "$#{field[0..(field.length - content_identifier.length)]}"
+
+            return "$#{field[0...(field.length - content_identifier.length)]}"
           end
 
           parent = field[0..field.rindex('.')]
+          parent = parent.gsub(/\.+$/, '') # Remove trailing dots
+
           ConditionGenerator.tag_record_if_not_exist_by_value(parent, "$#{field}")
         end
       end
---
+++ packages/forest_admin_datasource_mongoid/spec/lib/forest_admin_datasource_mongoid/utils/pipeline/virtual_field_generator_spec.rb
@@ -0,0 +1,96 @@
+require 'spec_helper'
+
+module ForestAdminDatasourceMongoid
+  module Utils
+    module Pipeline
+      describe VirtualFieldGenerator do
+        it 'does not add fields that are not within the projection' do
+          projection = []
+          stack = [{ prefix: nil, as_fields: [], as_models: ['author'] }]
+          pipeline = described_class.add_virtual(Post, stack, projection)
+
+          expect(pipeline).to eq([])
+        end
+
+        it 'adds virtual fields fake many to one' do
+          projection = ['author:_id', 'author:parent_id']
+          stack = [{ prefix: nil, as_fields: [], as_models: ['author'] }]
+          pipeline = described_class.add_virtual(Post, stack, projection)
+
+          expect(pipeline).to eq([
+                                   {
+                                     '$addFields' => {
+                                       'author._id' => ConditionGenerator.tag_record_if_not_exist_by_value('author', {
+                                                                                                             '$concat' => [{ '$toString' => '$_id' }, '.author']
+                                                                                                           }),
+                                       'author.parent_id' => ConditionGenerator.tag_record_if_not_exist_by_value('author', '$_id')
+                                     }
+                                   }
+                                 ])
+        end
+
+        it 'adds virtual fields on boxed many to one' do
+          projection = ['title:_id', 'title:parent_id', 'title:content']
+          stack = [{ prefix: nil, as_fields: [], as_models: ['title'] }]
+          pipeline = described_class.add_virtual(Post, stack, projection)
+
+          expect(pipeline).to eq([
+                                   {
+                                     '$addFields' => {
+                                       'title._id' => ConditionGenerator.tag_record_if_not_exist_by_value('title', {
+                                                                                                            '$concat' => [{ '$toString' => '$_id' }, '.title']
+                                                                                                          }),
+                                       'title.parent_id' => ConditionGenerator.tag_record_if_not_exist_by_value('title', '$_id'),
+                                       'title.content' => '$title'
+                                     }
+                                   }
+                                 ])
+        end
+
+        it 'adds nested dependencies besides for parent_id (for server-side queries only)' do
+          projection = ['author:country:_id']
+          stack = [{ prefix: nil, as_fields: [], as_models: ['author'] }]
+          pipeline = described_class.add_virtual(Post, stack, projection)
+
+          expect(pipeline).to eq([
+                                   {
+                                     '$addFields' => {
+                                       'author.country._id' => ConditionGenerator.tag_record_if_not_exist_by_value(
+                                         'author.country',
+                                         {
+                                           '$concat' => [{ '$toString' => '$_id' }, '.author.country']
+                                         }
+                                       )
+                                     }
+                                   }
+                                 ])
+        end
+
+        it 'adds nested dependencies' do
+          projection = ['author:country:name']
+          stack = [{ prefix: nil, as_fields: [], as_models: ['author'] }]
+          pipeline = described_class.add_virtual(Post, stack, projection)
+
+          expect(pipeline).to eq([
+                                   {
+                                     '$addFields' => {
+                                       'author.country.name' => ConditionGenerator.tag_record_if_not_exist_by_value(
+                                         'author.country',
+                                         '$author.country.name'
+                                       )
+                                     }
+                                   }
+                                 ])
+        end
+
+        it 'crashes on nested parent_id (for server-side queries only)' do
+          projection = ['author:country:parent_id']
+          stack = [{ prefix: nil, as_fields: [], as_models: ['author'] }]
+          expect do
+            described_class.add_virtual(Post, stack, projection)
+          end.to raise_error(ForestAdminDatasourceToolkit::Exceptions::ForestException, '🌳🌳🌳 Fetching virtual parent_id deeper than 1 level is not supported.')
+        end
+      end
+    end
+  end
+end
