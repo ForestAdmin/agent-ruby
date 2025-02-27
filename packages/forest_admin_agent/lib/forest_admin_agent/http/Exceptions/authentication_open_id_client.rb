81370ed5e25b8e4250de18b65e8c58afec042665
Matthieu <matthv@gmail.com>
2025-02-27 16:01:36 +0100

    fix: proper raise of OpenIDConnect Exceptions (#112)

--- packages/forest_admin_agent/lib/forest_admin_agent/http/Exceptions/authentication_open_id_client.rb
+++ packages/forest_admin_agent/lib/forest_admin_agent/http/Exceptions/authentication_open_id_client.rb
@@ -2,13 +2,10 @@ module ForestAdminAgent
   module Http
     module Exceptions
       class AuthenticationOpenIdClient < HttpException
-        attr_reader :error, :message, :state
-
-        def initialize(error, error_description, state)
-          super(error, 401, error_description)
-          @error = error
-          @message = error_description
-          @state = state
+        def initialize(status = 401,
+                       message = 'Authentication failed with OpenID Client',
+                       name = 'AuthenticationOpenIdClient')
+          super
         end
       end
     end
