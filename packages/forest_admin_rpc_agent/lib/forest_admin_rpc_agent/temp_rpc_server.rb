3803bde071a380b3ee1b2f0769ae214bcedffdc1
Matt <matthv@gmail.com>
2025-03-12 17:31:44 +0100

    feat: remove gRPC

--- packages/forest_admin_rpc_agent/lib/forest_admin_rpc_agent/temp_rpc_server.rb
+++ packages/forest_admin_rpc_agent/lib/forest_admin_rpc_agent/temp_rpc_server.rb
@@ -1,28 +1,18 @@
-require 'grpc'
-require_relative 'proto/rpc_services_pb'
+require 'sinatra'
+require 'json'
 
-class TempRpcServer < ForestAdminRpcAgent::Rpc::Service
-  def list_users(_empty, _call)
-    users = [
-      ForestAdminRpcAgent::User.new(id: '1', email: 'rick@sanchez.com', first_name: 'Rick', last_name: 'Sanchez'),
-      ForestAdminRpcAgent::User.new(id: '2', email: 'morty@smith.com', first_name: 'Morty', last_name: 'Smith'),
-      ForestAdminRpcAgent::User.new(id: '3', email: 'summer@smith.com', first_name: 'Summer', last_name: 'Smith'),
-      ForestAdminRpcAgent::User.new(id: '4', email: 'beth@smith.com', first_name: 'Beth', last_name: 'Smith'),
-      ForestAdminRpcAgent::User.new(id: '5', email: 'jerry@smith.com', first_name: 'Jerry', last_name: 'Smith')
-    ]
+set :port, 5000
 
-    ForestAdminRpcAgent::UserList.new(users: users)
-  end
+get '/users' do
+  content_type :json
+
+  [
+    { id: '1', email: 'rick@sanchez.com', first_name: 'Rick', last_name: 'Sanchez' },
+    { id: '2', email: 'morty@smith.com', first_name: 'Morty', last_name: 'Smith' },
+    { id: '3', email: 'summer@smith.com', first_name: 'Summer', last_name: 'Smith' },
+    { id: '4', email: 'beth@smith.com', first_name: 'Beth', last_name: 'Smith' },
+    { id: '5', email: 'jerry@smith.com', first_name: 'Jerry', last_name: 'Smith' }
+  ].to_json
 end
 
-def main
-  address = 'localhost:50051'
-  server = GRPC::RpcServer.new
-  server.add_http2_port(address, :this_port_is_insecure)
-  server.handle(TempRpcServer.new)
-
-  puts "✅ gRPC Server running on #{address}"
-  server.run_till_terminated
-end
-
-main if __FILE__ == $PROGRAM_NAME
+puts '✅ HTTP Server running on http://localhost:5000'
