1a2908dc2247c723098f9bce3063e77a6bde47a1
Nicolas Alexandre <nicolasalexandre9@gmail.com>
2025-03-17 16:15:56 +0100

    feat: add CRUD functions on collection rpc

--- packages/forest_admin_datasource_rpc/lib/forest_admin_datasource_rpc/collection.rb
+++ packages/forest_admin_datasource_rpc/lib/forest_admin_datasource_rpc/collection.rb
@@ -3,7 +3,8 @@ module ForestAdminDatasourceRpc
     def initialize(datasource, name, options, schema)
       super(datasource, name)
       @options = options
-      @rpc_collection_uri = "#{options["uri"]}/forest/rpc/#{name}"
+      @rpc_collection_uri = "#{options[:uri]}/forest/rpc/#{name}"
+      @client = Utils::ApiRequester.new(@rpc_collection_uri, @options[:token])
 
       ForestAdminAgent::Facades::Container.logger.log('Debug', "Create Rpc collection #{name}.")
 
@@ -11,41 +12,114 @@ module ForestAdminDatasourceRpc
       enable_search if schema[:searchable]
 
       schema[:actions].each { |action_name, action_schema| add_action(action_name, action_schema) }
-      schema[:charts].each { |chart| add_chart(chart) }
+      # schema[:charts].each { |chart| add_chart(chart) }
       schema[:fields].each { |field_name, field_schema| add_field(field_name, field_schema) }
       add_segments(schema[:segments])
     end
 
     def list(caller, filter, projection)
-      # TODO
+      params = { caller: caller.to_h, filter: filter.to_h, projection: projection, timezone: caller.timezone }
+      # url = "/list?#{URI.encode_www_form(params)}"
+      url = '/list'
+
+      ForestAdminAgent::Facades::Container.logger.log(
+        'Debug',
+        "Forwarding '#{@name}' list call to the Rpc agent on #{url}."
+      )
+
+      response = @client.get(url, params)
+      response.body
     end
 
     def create(caller, data)
-      # TODO
+      params = { caller: caller.to_h, timezone: caller.timezone, data: data }
+      url = '/create'
+
+      ForestAdminAgent::Facades::Container.logger.log(
+        'Debug',
+        "Forwarding '#{@name}' creation call to the Rpc agent on #{url}."
+      )
+
+      response = @client.post(url, params)
+      response.body
     end
 
     def update(caller, filter, data)
-      # TODO
+      params = { caller: caller.to_h, timezone: caller.timezone, filter: filter, data: data }
+      url = '/update'
+
+      ForestAdminAgent::Facades::Container.logger.log(
+        'Debug',
+        "Forwarding '#{@name}' update call to the Rpc agent on #{url}."
+      )
+
+      @client.put(url, params)
     end
 
     def delete(caller, filter)
-      # TODO
+      params = { caller: caller.to_h, timezone: caller.timezone, filter: filter }
+      url = '/delete'
+
+      ForestAdminAgent::Facades::Container.logger.log(
+        'Debug',
+        "Forwarding '#{@name}' delete call to the Rpc agent on #{url}."
+      )
+
+      @client.delete(url, params)
     end
 
     def aggregate(caller, filter, aggregation, limit = nil)
-      # TODO
+      params = { caller: caller.to_h, timezone: caller.timezone, filter: filter, aggregation: aggregation,
+                 limit: limit }
+      url = '/aggregate'
+
+      ForestAdminAgent::Facades::Container.logger.log(
+        'Debug',
+        "Forwarding '#{@name}' aggregation call to the Rpc agent on #{url}."
+      )
+
+      response = @client.get(url, params)
+      response.body
     end
 
-    def execute(caller, name, data, filter = nil)
-      # TODO
+    def execute(caller, name, _data, filter = nil)
+      params = { caller: caller.to_h, timezone: caller.timezone, filter: filter, name: name }
+      url = '/action-execute'
+
+      ForestAdminAgent::Facades::Container.logger.log(
+        'Debug',
+        "Forwarding '#{@name}' action #{name} call to the Rpc agent on #{url}."
+      )
+
+      response = @client.post(url, params)
+      # TODO: action with file
+      response.body
     end
 
     def get_form(caller, name, data = nil, filter = nil, metas = nil)
-      # TODO
+      params = { caller: caller.to_h, timezone: caller.timezone, filter: filter, name: name, metas: metas, data: data }
+      url = '/action-form'
+
+      ForestAdminAgent::Facades::Container.logger.log(
+        'Debug',
+        "Forwarding '#{@name}' action form #{name} call to the Rpc agent on #{url}."
+      )
+
+      response = @client.post(url, params)
+      response.body
     end
 
-    def render_chart(_caller, name, _record_id)
-      # TODO
+    def render_chart(caller, name, record_id)
+      params = { caller: caller.to_h, timezone: caller.timezone, name: name, record_id: record_id }
+      url = '/chart'
+
+      ForestAdminAgent::Facades::Container.logger.log(
+        'Debug',
+        "Forwarding '#{@name}' chart #{name} call to the Rpc agent on #{url}."
+      )
+
+      response = @client.get(url, params)
+      response.body
     end
   end
 end
