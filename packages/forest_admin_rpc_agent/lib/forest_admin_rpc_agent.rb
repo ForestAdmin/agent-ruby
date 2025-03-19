9f74285eb0326d32500248e6a1114b1d6bd9b020
Matt <matthv@gmail.com>
2025-03-19 14:28:57 +0100

    chore: clean config

--- packages/forest_admin_rpc_agent/lib/forest_admin_rpc_agent.rb
+++ packages/forest_admin_rpc_agent/lib/forest_admin_rpc_agent.rb
@@ -18,14 +18,11 @@ module ForestAdminRpcAgent
   setting :forest_server_url, default: 'https://api.forestadmin.com'
   setting :is_production, default: false
   setting :prefix, default: nil
-  setting :permission_expiration, default: 900
   setting :cache_dir, default: :'tmp/cache/forest_admin'
-  setting :schema_path, default: File.join(Dir.pwd, '.forestadmin-schema.json')
   setting :project_dir, default: Dir.pwd
   setting :loggerLevel, default: 'info'
   setting :logger, default: nil
   setting :customize_error_message, default: nil
-  setting :instant_cache_refresh, default: false
 
   begin
     require 'thor'
