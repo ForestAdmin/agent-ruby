e41b42f28aa20b67af5a7f16fbb50738b3b697c5
Matt <matthv@gmail.com>
2025-03-19 11:55:07 +0100

    feat(rpc): handle midleware authentication - valid signature with timestamp

--- packages/forest_admin_rpc_agent/lib/forest_admin_rpc_agent/middleware/authentication.rb
+++ packages/forest_admin_rpc_agent/lib/forest_admin_rpc_agent/middleware/authentication.rb
@@ -1,6 +1,9 @@
 module ForestAdminRpcAgent
   module Middleware
     class Authentication
+      ALLOWED_TIME_DIFF = 300
+      @@used_signatures = {}
+
       def initialize(app)
         @app = app
       end
@@ -8,11 +11,9 @@ module ForestAdminRpcAgent
       def call(env)
         request = Rack::Request.new(env)
         signature = request.get_header('HTTP_X_SIGNATURE')
-        # TODO: IMPLEMENTATION HTTP_X_TIMESTAMP (for replay attack)
-        body = request.body.read
-        request.body.rewind
+        timestamp = request.get_header('HTTP_X_TIMESTAMP')
 
-        unless valid_signature?(signature, body)
+        unless valid_signature?(signature, timestamp)
           return [401, { 'Content-Type' => 'application/json' }, [{ error: 'Unauthorized' }.to_json]]
         end
 
@@ -21,11 +22,39 @@ module ForestAdminRpcAgent
 
       private
 
-      def valid_signature?(signature, body)
-        return false if signature.nil? || body.nil?
+      def valid_signature?(signature, timestamp)
+        return false if signature.nil? || timestamp.nil?
 
-        expected_signature = OpenSSL::HMAC.hexdigest('SHA256', auth_secret, body)
-        Rack::Utils.secure_compare(signature, expected_signature)
+        return false unless valid_timestamp?(timestamp)
+
+        expected_signature = OpenSSL::HMAC.hexdigest('SHA256', auth_secret, timestamp)
+
+        return false unless Rack::Utils.secure_compare(signature, expected_signature)
+
+        # check if this signature has already been used (replay attack)
+        return false if @@used_signatures.key?(signature)
+
+        @@used_signatures[signature] = Time.now.utc.to_i
+
+        cleanup_old_signatures
+
+        true
+      end
+
+      def valid_timestamp?(timestamp)
+        time = begin
+          Time.iso8601(timestamp)
+        rescue StandardError
+          nil
+        end
+        return false if time.nil?
+
+        (Time.now.utc.to_i - time.to_i).abs <= ALLOWED_TIME_DIFF
+      end
+
+      def cleanup_old_signatures
+        now = Time.now.utc.to_i
+        @@used_signatures.delete_if { |_key, timestamp| now - timestamp > ALLOWED_TIME_DIFF }
       end
 
       def auth_secret
