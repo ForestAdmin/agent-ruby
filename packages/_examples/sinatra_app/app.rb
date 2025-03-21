0f42e97cdf6b112db26607e92fd08625bcc1e88a
Nicolas Alexandre <nicolasalexandre9@gmail.com>
2025-03-21 15:17:52 +0100

    feat: update sinatra app example

--- packages/_examples/sinatra_app/app.rb
+++ packages/_examples/sinatra_app/app.rb
@@ -1,14 +1,22 @@
 require 'bundler/setup'
 Bundler.require
 
-require 'sinatra'
 require 'sinatra/activerecord'
+require 'sinatra'
+require_relative 'config/forest_admin_sinatra'
+require 'forest_admin_sinatra/extensions/sinatra_extension'
 
 # load models
 Dir["models/*.rb"].each {|file| require_relative file.remove('.rb') }
 
 # setup databse
 set :database, {adapter: "sqlite3", database: "database.sqlite3"}
+
+# setup FOREST AGENT
+database_configuration = ActiveRecord::Base.connection_db_config
+datasource = ForestAdminDatasourceActiveRecord::Datasource.new(database_configuration)
+agent = ForestAdminAgent::Builder::AgentFactory.instance.add_datasource(datasource)
+agent.build
 
 get '/' do
   'Welcome on forest Sinatra app example'
