29e9c807406786e469105d1d8b0d066b853c31fd
Matt <matthv@gmail.com>
2025-03-24 11:42:16 +0100

    chore: add collection chart example

--- packages/_examples/warehouse/app/lib/forest_admin_rpc_agent/create_rpc_agent.rb
+++ packages/_examples/warehouse/app/lib/forest_admin_rpc_agent/create_rpc_agent.rb
@@ -3,12 +3,39 @@
 
 module ForestAdminRpcAgent
   class CreateRpcAgent
+    include ForestAdminDatasourceToolkit::Components::Query
+    include ForestAdminDatasourceToolkit::Components::Query::ConditionTree
+
     def self.setup!
       datasource = ForestAdminDatasourceActiveRecord::Datasource.new(Rails.env.to_sym)
 
-      ForestAdminRpcAgent::Agent.instance
+      agent = ForestAdminRpcAgent::Agent.instance
                                 .add_datasource(datasource)
-                                .build
+
+
+      agent.customize_collection('Product') do |collection|
+        collection.add_chart('groupByManufacturer') do |context, result_builder|
+          aggregation = Aggregation.new(operation: 'Count', field: 'manufacturer:id')
+          filter = Filter.new(
+            condition_tree: Nodes::ConditionTreeBranch.new(
+              'And',
+              [
+                Nodes::ConditionTreeLeaf.new('manufacturer:id', Operators::PRESENT),
+              ]
+            )
+          )
+          result = context.collection.aggregate(filter, aggregation)
+
+          result_builder.value(result[0]['value']).to_json
+        end
+      end
+
+      # @agent.add_chart('appointments') do |_context, result_builder|
+      #   result_builder.value(784, 760)
+      # end
+
+      agent.build
+
     end
   end
 end
