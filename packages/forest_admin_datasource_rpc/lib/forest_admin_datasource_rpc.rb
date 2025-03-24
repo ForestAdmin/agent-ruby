7613e69b5f32fec439d526bd36a020eadc73a68b
Matt <matthv@gmail.com>
2025-03-24 17:40:30 +0100

    chore: add new log that check the RPC agent is running

--- packages/forest_admin_datasource_rpc/lib/forest_admin_datasource_rpc.rb
+++ packages/forest_admin_datasource_rpc/lib/forest_admin_datasource_rpc.rb
@@ -11,10 +11,15 @@ module ForestAdminDatasourceRpc
 
   def self.build(options)
     uri = options[:uri]
-    ForestAdminRpcAgent::Facades::Container.logger.log('Info', "Getting schema from Rpc agent on #{uri}.")
+    ForestAdminRpcAgent::Facades::Container.logger.log('Info', "Getting schema from RPC agent on #{uri}.")
 
-    schema = Utils::RpcClient.new(uri, ForestAdminAgent::Facades::Container.cache(:auth_secret))
-                             .call_rpc('/forest/rpc-schema', method: :get, symbolize_keys: true)
+    begin
+      schema = Utils::RpcClient.new(uri, ForestAdminAgent::Facades::Container.cache(:auth_secret))
+                               .call_rpc('/forest/rpc-schema', method: :get, symbolize_keys: true)
+    rescue StandardError
+      raise ForestAdminDatasourceToolkit::Exceptions::ForestException,
+            'Failed to get schema from RPC agent. Please check the RPC agent is running.'
+    end
 
     ForestAdminDatasourceRpc::Datasource.new(options, schema)
   end
