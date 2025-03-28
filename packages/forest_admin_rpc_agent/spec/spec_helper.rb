36844e89853ce58417aa244017b2de5178d17db6
Matt <matthv@gmail.com>
2025-03-28 16:06:59 +0100

    chore: add test

--- packages/forest_admin_rpc_agent/spec/spec_helper.rb
+++ packages/forest_admin_rpc_agent/spec/spec_helper.rb
@@ -1,6 +1,10 @@
 require 'simplecov'
 require 'simplecov_json_formatter'
 require 'simplecov-html'
+require 'forest_admin_rpc_agent'
+require 'forest_admin_datasource_customizer'
+require 'forest_admin_test_toolkit'
+require 'singleton'
 
 SimpleCov.formatters = [SimpleCov::Formatter::JSONFormatter, SimpleCov::Formatter::HTMLFormatter]
 SimpleCov.start do
@@ -13,4 +17,10 @@ SimpleCov.at_exit do
   result.format!
 end
 
-# TODO
+RSpec.shared_context 'with caller' do
+  let(:caller) { build_caller }
+end
+
+RSpec.configure do |config|
+  config.include ForestAdminTestToolkit::Factory::Caller
+end
