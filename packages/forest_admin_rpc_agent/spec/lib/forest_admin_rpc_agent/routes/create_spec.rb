589b1fd266095067645c1e9ca2c1ef93790dd474
Matt <matthv@gmail.com>
2025-03-31 15:26:32 +0200

    chore: add crud test on rpc package

--- packages/forest_admin_rpc_agent/spec/lib/forest_admin_rpc_agent/routes/create_spec.rb
+++ packages/forest_admin_rpc_agent/spec/lib/forest_admin_rpc_agent/routes/create_spec.rb
@@ -3,48 +3,35 @@ require 'spec_helper'
 module ForestAdminRpcAgent
   module Routes
     include ForestAdminDatasourceRpc
-    describe List do
+    describe Create do
       include_context 'with caller'
 
       let(:route) { described_class.new }
       let(:collection_name) { 'users' }
-      let(:filter_params) { { 'field' => 'email', 'operator' => 'equal', 'value' => 'test@example.com' } }
-      let(:projection_params) { %w[id email] }
+      let(:data) { { 'email' => 'test@example.com', 'name' => 'John Doe' } }
       let(:params) do
         {
           'collection_name' => collection_name,
           'caller' => caller.to_h,
-          'filter' => filter_params,
-          'projection' => projection_params
+          'data' => data
         }
       end
-      let(:datasource) { instance_double(Datasource) }
+      let(:datasource) { instance_double(ForestAdminDatasourceRpc::Datasource) }
       let(:collection) { instance_double(Collection) }
-      let(:projection) { instance_double(ForestAdminDatasourceToolkit::Components::Query::Projection) }
-      let(:filter) { instance_double(ForestAdminDatasourceToolkit::Components::Query::Filter) }
-      let(:list_result) { [{ id: 1, email: 'test@example.com' }] }
+      let(:create_result) { { id: 1, email: 'test@example.com', name: 'John Doe' } }
 
       before do
         allow(ForestAdminDatasourceToolkit::Components::Caller).to receive(:new).and_return(caller)
         allow(ForestAdminRpcAgent::Facades::Container).to receive(:datasource).and_return(datasource)
         allow(datasource).to receive(:get_collection).with(collection_name).and_return(collection)
-
-        allow(ForestAdminDatasourceToolkit::Components::Query::Projection).to receive(:new)
-          .with(projection_params)
-          .and_return(projection)
-
-        allow(ForestAdminDatasourceToolkit::Components::Query::FilterFactory).to receive(:from_plain_object)
-          .with(filter_params)
-          .and_return(filter)
-
-        allow(collection).to receive(:list).with(caller, filter, projection).and_return(list_result)
+        allow(collection).to receive(:create).with(caller, data).and_return(create_result)
       end
 
       describe '#handle_request' do
         context 'when collection_name is provided' do
-          it 'returns the list of items as JSON' do
+          it 'creates a new record and returns it as JSON' do
             result = route.handle_request(params: params)
-            expect(result).to eq(list_result.to_json)
+            expect(result).to eq(create_result.to_json)
           end
         end
 
--- packages/forest_admin_rpc_agent/spec/lib/forest_admin_rpc_agent/routes/update_spec.rb
+++ packages/forest_admin_rpc_agent/spec/lib/forest_admin_rpc_agent/routes/update_spec.rb
@@ -3,48 +3,43 @@ require 'spec_helper'
 module ForestAdminRpcAgent
   module Routes
     include ForestAdminDatasourceRpc
-    describe List do
+    describe Update do
       include_context 'with caller'
 
       let(:route) { described_class.new }
       let(:collection_name) { 'users' }
-      let(:filter_params) { { 'field' => 'email', 'operator' => 'equal', 'value' => 'test@example.com' } }
-      let(:projection_params) { %w[id email] }
+      let(:filter_params) { { 'field' => 'id', 'operator' => 'equals', 'value' => 1 } }
+      let(:update_data) { { 'email' => 'updated@example.com' } }
       let(:params) do
         {
           'collection_name' => collection_name,
           'caller' => caller.to_h,
           'filter' => filter_params,
-          'projection' => projection_params
+          'data' => update_data
         }
       end
       let(:datasource) { instance_double(Datasource) }
       let(:collection) { instance_double(Collection) }
-      let(:projection) { instance_double(ForestAdminDatasourceToolkit::Components::Query::Projection) }
       let(:filter) { instance_double(ForestAdminDatasourceToolkit::Components::Query::Filter) }
-      let(:list_result) { [{ id: 1, email: 'test@example.com' }] }
+      let(:update_result) { nil }
 
       before do
         allow(ForestAdminDatasourceToolkit::Components::Caller).to receive(:new).and_return(caller)
         allow(ForestAdminRpcAgent::Facades::Container).to receive(:datasource).and_return(datasource)
         allow(datasource).to receive(:get_collection).with(collection_name).and_return(collection)
 
-        allow(ForestAdminDatasourceToolkit::Components::Query::Projection).to receive(:new)
-          .with(projection_params)
-          .and_return(projection)
-
         allow(ForestAdminDatasourceToolkit::Components::Query::FilterFactory).to receive(:from_plain_object)
           .with(filter_params)
           .and_return(filter)
 
-        allow(collection).to receive(:list).with(caller, filter, projection).and_return(list_result)
+        allow(collection).to receive(:update).with(caller, filter, update_data).and_return(update_result)
       end
 
       describe '#handle_request' do
         context 'when collection_name is provided' do
-          it 'returns the list of items as JSON' do
+          it 'updates the collection and returns the result as JSON' do
             result = route.handle_request(params: params)
-            expect(result).to eq(list_result.to_json)
+            expect(result).to eq(update_result.to_json)
           end
         end
 
