8bfc775590aa43556276933da58f8aaa8a25ba8c
Matt <matthv@gmail.com>
2025-06-20 11:32:21 +0200

    refactor: spec_helper

--- packages/forest_admin_datasource_active_record/spec/spec_helper.rb
+++ packages/forest_admin_datasource_active_record/spec/spec_helper.rb
@@ -33,18 +33,13 @@ RSpec.configure do |config|
 
   # database_cleaner config
   config.before(:suite) do
-    DatabaseCleaner.clean_with :truncation, except: %w[ar_internal_metadata]
-  end
-
-  config.before do
     DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with :truncation
   end
 
-  config.before do
-    DatabaseCleaner.start
-  end
-
-  config.after do
-    DatabaseCleaner.clean
+  config.around do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
   end
 end
