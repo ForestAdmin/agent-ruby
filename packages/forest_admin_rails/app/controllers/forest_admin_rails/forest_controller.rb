3a41dd2914163cb1d8b9c55486bef8bb1c136828
Matthieu <matthv@gmail.com>
2025-10-10 16:09:49 +0200

    fix(controller): simplify forest_controller exception_handler (#141)

--- packages/forest_admin_rails/app/controllers/forest_admin_rails/forest_controller.rb
+++ packages/forest_admin_rails/app/controllers/forest_admin_rails/forest_controller.rb
@@ -49,29 +49,23 @@ module ForestAdminRails
                  state: exception.status
                }
              else
-               if exception.respond_to?(:name) && exception.name.present?
-                 {
-                   errors: [
-                     {
-                       name: exception.name,
-                       detail: get_error_message(exception),
-                       status: exception.try(:status),
-                       data: exception.try(:data)
-                     }
-                   ]
-                 }
-               else
-                 {
-                   error: exception.message
-                 }
-               end
+               {
+                 errors: [
+                   {
+                     name: exception.respond_to?(:name) ? exception.name : exception.class.name,
+                     detail: get_error_message(exception),
+                     status: exception.try(:status),
+                     data: exception.try(:data)
+                   }
+                 ]
+               }
              end
 
       unless ForestAdminAgent::Facades::Container.cache(:is_production)
         ForestAdminAgent::Facades::Container.logger.log('Debug', exception.full_message)
       end
 
-      render json: data, status: exception.try(:status)
+      render json: data, status: exception.try(:status) || 500
     end
   end
 end
