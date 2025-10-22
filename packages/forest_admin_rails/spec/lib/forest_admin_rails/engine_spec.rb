63ea2b193922201ab5afd9b1bdd2a52c65a9838b
Matt <matthv@gmail.com>
2025-10-22 16:15:12 +0200

    chore: add tests

--- packages/forest_admin_rails/spec/lib/forest_admin_rails/engine_spec.rb
+++ packages/forest_admin_rails/spec/lib/forest_admin_rails/engine_spec.rb
@@ -6,84 +6,218 @@ module ForestAdminRails
     def self.setup!; end
   end
 
-  RSpec.describe 'Engine#load_configuration' do
-    describe 'Rails::Server presence check' do
-      context 'when Rails::Server is not defined' do
-        before { hide_const('Rails::Server') }
+  RSpec.describe 'Engine#running_web_server?' do
+    let(:original_argv) { ARGV.dup }
+    let(:original_program_name) { $PROGRAM_NAME }
 
-        it 'returns early without calling setup when Rails::Server is not defined' do
-          # This simulates the behavior when running rails console, rake tasks, migrations, etc.
-          return_value = nil
-          return_value = true unless defined?(::Rails::Server)
+    before do
+      # Hide Rails::Server constant by default
+      hide_const('Rails::Server') if defined?(Rails::Server)
+    end
 
-          expect(return_value).to be true
-        end
+    after do
+      # Restore original values
+      ARGV.replace(original_argv)
+      $PROGRAM_NAME = original_program_name
+    end
 
-        it 'does not call setup when running rake tasks' do
-          hide_const('Rails::Server')
-          create_agent_class = class_double(CreateAgent, setup!: nil)
-          stub_const('ForestAdminRails::CreateAgent', create_agent_class)
+    describe 'web server detection' do
+      context 'when Rails::Server is defined' do
+        it 'returns true for development server' do
+          stub_const('Rails::Server', Class.new)
 
-          # The condition should prevent setup from being called
-          create_agent_class.setup! if defined?(::Rails::Server)
+          result = defined?(::Rails::Server)
 
-          expect(create_agent_class).not_to have_received(:setup!)
-        end
-
-        it 'does not call setup when running "rails db:migrate"' do
-          hide_const('Rails::Server')
-          stub_const('ARGV', ['db:migrate'])
-          create_agent_class = class_double(CreateAgent, setup!: nil)
-          stub_const('ForestAdminRails::CreateAgent', create_agent_class)
-
-          create_agent_class.setup! if defined?(::Rails::Server)
-
-          expect(create_agent_class).not_to have_received(:setup!)
-        end
-
-        it 'does not call setup when running "rails console"' do
-          hide_const('Rails::Server')
-          stub_const('Rails::Console', Class.new)
-          create_agent_class = class_double(CreateAgent, setup!: nil)
-          stub_const('ForestAdminRails::CreateAgent', create_agent_class)
-
-          create_agent_class.setup! if defined?(::Rails::Server)
-
-          expect(create_agent_class).not_to have_received(:setup!)
+          expect(result).to be_truthy
         end
       end
 
-      context 'when Rails::Server is defined (running rails server)' do
-        it 'allows setup to be called' do
-          stub_const('Rails::Server', Class.new)
-          create_agent_class = class_double(CreateAgent, setup!: nil)
-          stub_const('ForestAdminRails::CreateAgent', create_agent_class)
+      context 'when server command is in ARGV' do
+        it 'returns true when running puma' do
+          ARGV.replace(['puma', '-C', 'config/puma.rb'])
 
-          should_continue = defined?(::Rails::Server)
-          create_agent_class.setup! if should_continue
+          server_commands = %w[puma unicorn thin passenger rackup]
+          result = server_commands.any? { |cmd| ARGV.any? { |arg| arg.include?(cmd) } }
 
-          expect(create_agent_class).to have_received(:setup!)
+          expect(result).to be true
+        end
+
+        it 'returns true when running unicorn' do
+          ARGV.replace(['unicorn', '-c', 'config/unicorn.rb'])
+
+          server_commands = %w[puma unicorn thin passenger rackup]
+          result = server_commands.any? { |cmd| ARGV.any? { |arg| arg.include?(cmd) } }
+
+          expect(result).to be true
+        end
+
+        it 'returns true when running thin' do
+          ARGV.replace(['thin', 'start'])
+
+          server_commands = %w[puma unicorn thin passenger rackup]
+          result = server_commands.any? { |cmd| ARGV.any? { |arg| arg.include?(cmd) } }
+
+          expect(result).to be true
+        end
+
+        it 'returns true when running passenger' do
+          ARGV.replace(['passenger', 'start'])
+
+          server_commands = %w[puma unicorn thin passenger rackup]
+          result = server_commands.any? { |cmd| ARGV.any? { |arg| arg.include?(cmd) } }
+
+          expect(result).to be true
         end
       end
 
-      context 'with file existence check' do
-        it 'returns early when create_agent.rb does not exist' do
-          stub_const('Rails::Server', Class.new)
-          file_exists = false
+      context 'when server name is in $PROGRAM_NAME' do
+        it 'returns true when running via puma executable' do
+          $PROGRAM_NAME = '/usr/local/bin/puma'
 
-          should_skip = !defined?(::Rails::Server) || !file_exists
+          result = $PROGRAM_NAME.match?(/puma|unicorn|thin|passenger/)
 
-          expect(should_skip).to be true
+          expect(result).to be true
         end
 
-        it 'continues when create_agent.rb exists and Rails::Server is defined' do
-          stub_const('Rails::Server', Class.new)
-          file_exists = true
+        it 'returns true when running via unicorn executable' do
+          $PROGRAM_NAME = '/usr/local/bin/unicorn'
 
-          should_skip = !defined?(::Rails::Server) || !file_exists
+          result = $PROGRAM_NAME.match?(/puma|unicorn|thin|passenger/)
 
-          expect(should_skip).to be false
+          expect(result).to be true
         end
+
+        it 'returns true when running puma -C config/puma.rb' do
+          # When running: puma -C config/puma.rb
+          # ARGV will be: ['-C', 'config/puma.rb']
+          # $PROGRAM_NAME will be something like: '/usr/local/bundle/bin/puma' or 'puma'
+          ARGV.replace(['-C', 'config/puma.rb'])
+          $PROGRAM_NAME = '/usr/local/bundle/bin/puma'
+
+          server_commands = %w[puma unicorn thin passenger rackup]
+          result = server_commands.any? { |cmd| ARGV.any? { |arg| arg.include?(cmd) } } ||
+                   $PROGRAM_NAME.match?(/puma|unicorn|thin|passenger/)
+
+          expect(result).to be true
+        end
+      end
+
+      context 'when no web server is detected' do
+        it 'returns false for console' do
+          hide_const('Rails::Server')
+          ARGV.replace(['console'])
+          $PROGRAM_NAME = 'rails'
+
+          server_commands = %w[puma unicorn thin passenger rackup]
+          result = defined?(::Rails::Server) ||
+                   server_commands.any? { |cmd| ARGV.any? { |arg| arg.include?(cmd) } } ||
+                   $PROGRAM_NAME.match?(/puma|unicorn|thin|passenger/)
+
+          expect(result).to be_falsy
+        end
+
+        it 'returns false for rake tasks' do
+          hide_const('Rails::Server')
+          ARGV.replace(['db:migrate'])
+          $PROGRAM_NAME = 'rake'
+
+          server_commands = %w[puma unicorn thin passenger rackup]
+          result = defined?(::Rails::Server) ||
+                   server_commands.any? { |cmd| ARGV.any? { |arg| arg.include?(cmd) } } ||
+                   $PROGRAM_NAME.match?(/puma|unicorn|thin|passenger/)
+
+          expect(result).to be_falsy
+        end
+
+        it 'returns false for migrations' do
+          hide_const('Rails::Server')
+          ARGV.replace(['db:migrate'])
+          $PROGRAM_NAME = 'rails'
+
+          server_commands = %w[puma unicorn thin passenger rackup]
+          result = defined?(::Rails::Server) ||
+                   server_commands.any? { |cmd| ARGV.any? { |arg| arg.include?(cmd) } } ||
+                   $PROGRAM_NAME.match?(/puma|unicorn|thin|passenger/)
+
+          expect(result).to be_falsy
+        end
+
+        it 'returns false for rails about' do
+          hide_const('Rails::Server')
+          ARGV.replace(['about'])
+          $PROGRAM_NAME = 'rails'
+
+          server_commands = %w[puma unicorn thin passenger rackup]
+          result = defined?(::Rails::Server) ||
+                   server_commands.any? { |cmd| ARGV.any? { |arg| arg.include?(cmd) } } ||
+                   $PROGRAM_NAME.match?(/puma|unicorn|thin|passenger/)
+
+          expect(result).to be_falsy
+        end
+      end
+    end
+
+    describe 'combined server detection logic' do
+      it 'detects Rails development server' do
+        stub_const('Rails::Server', Class.new)
+
+        is_web_server = defined?(::Rails::Server)
+
+        expect(is_web_server).to be_truthy
+      end
+
+      it 'detects Puma via ARGV' do
+        ARGV.replace(['puma'])
+        $PROGRAM_NAME = 'bundle'
+
+        server_commands = %w[puma unicorn thin passenger rackup]
+        is_web_server = server_commands.any? { |cmd| ARGV.any? { |arg| arg.include?(cmd) } }
+
+        expect(is_web_server).to be_truthy
+      end
+
+      it 'detects Puma via $PROGRAM_NAME' do
+        ARGV.clear
+        $PROGRAM_NAME = '/usr/bin/puma'
+
+        is_web_server = $PROGRAM_NAME.match?(/puma|unicorn|thin|passenger/)
+
+        expect(is_web_server).to be_truthy
+      end
+
+      it 'returns false when no server is running' do
+        hide_const('Rails::Server')
+        ARGV.replace(['console'])
+        $PROGRAM_NAME = 'rails'
+
+        server_commands = %w[puma unicorn thin passenger rackup]
+        is_web_server = defined?(::Rails::Server) ||
+                        server_commands.any? { |cmd| ARGV.any? { |arg| arg.include?(cmd) } } ||
+                        $PROGRAM_NAME.match?(/puma|unicorn|thin|passenger/)
+
+        expect(is_web_server).to be_falsy
+      end
+    end
+
+    context 'with file existence check' do
+      it 'returns early when create_agent.rb does not exist' do
+        stub_const('Rails::Server', Class.new)
+        file_exists = false
+
+        should_skip = !defined?(::Rails::Server) || !file_exists
+
+        expect(should_skip).to be true
+      end
+
+      it 'continues when create_agent.rb exists and web server is running' do
+        ARGV.replace(['puma'])
+        file_exists = true
+
+        server_commands = %w[puma unicorn thin passenger rackup]
+        server_detected = server_commands.any? { |cmd| ARGV.any? { |arg| arg.include?(cmd) } }
+        should_skip = !server_detected || !file_exists
+
+        expect(should_skip).to be false
       end
     end
   end
