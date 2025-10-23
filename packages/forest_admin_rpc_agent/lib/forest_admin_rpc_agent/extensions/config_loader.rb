e01a19aee3a9e056affd845586c4194e30213423
Matt <matthv@gmail.com>
2025-10-23 14:52:03 +0200

    fix: move create_agent files to lib directory for consistency

--- packages/forest_admin_rpc_agent/lib/forest_admin_rpc_agent/extensions/config_loader.rb
+++ packages/forest_admin_rpc_agent/lib/forest_admin_rpc_agent/extensions/config_loader.rb
@@ -2,7 +2,7 @@ module ForestAdminRpcAgent
   module Extensions
     module ConfigLoader
       def self.load_configuration
-        config_file = File.join(Dir.pwd, 'app', 'lib', 'forest_admin_rpc_agent', 'create_rpc_agent.rb')
+        config_file = File.join(Dir.pwd, 'lib', 'forest_admin_rpc_agent', 'create_rpc_agent.rb')
         return unless File.exist?(config_file)
 
         ForestAdminRpcAgent::CreateRpcAgent.setup!
