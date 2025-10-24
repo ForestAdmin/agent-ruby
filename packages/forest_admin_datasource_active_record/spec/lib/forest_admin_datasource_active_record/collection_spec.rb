7a0857d8f0e7bf5691a06abb10aa5fe53f20884d
Dogan AY <dogan.ay@forestadmin.com>
2025-10-24 17:11:16 +0200

    fix: ci

--- packages/forest_admin_datasource_active_record/spec/lib/forest_admin_datasource_active_record/collection_spec.rb
+++ packages/forest_admin_datasource_active_record/spec/lib/forest_admin_datasource_active_record/collection_spec.rb
@@ -75,16 +75,18 @@ module ForestAdminDatasourceActiveRecord
           stub_const('CustomPkModel1', Class.new(ApplicationRecord) do
             self.table_name = 'cars'
             self.primary_key = 'reference'
+            self.abstract_class = true
 
             has_one :related_model, class_name: 'RelatedModel1', foreign_key: 'car_reference', primary_key: 'reference'
           end)
 
           stub_const('RelatedModel1', Class.new(ApplicationRecord) do
             self.table_name = 'users'
+            self.abstract_class = true
           end)
 
           datasource = Datasource.new({ adapter: 'sqlite3', database: 'db/database.db' })
-          collection = datasource.get_collection('CustomPkModel1')
+          collection = described_class.new(datasource, CustomPkModel1)
           relation_schema = collection.schema[:fields]['related_model']
 
           # The origin_key_target should be the custom primary_key specified in the association
@@ -95,16 +97,18 @@ module ForestAdminDatasourceActiveRecord
           stub_const('CustomPkModel2', Class.new(ApplicationRecord) do
             self.table_name = 'cars'
             self.primary_key = 'reference'
+            self.abstract_class = true
           end)
 
           stub_const('RelatedModel2', Class.new(ApplicationRecord) do
             self.table_name = 'users'
+            self.abstract_class = true
 
             belongs_to :custom_pk_model, class_name: 'CustomPkModel2', foreign_key: 'car_id', primary_key: 'reference'
           end)
 
           datasource = Datasource.new({ adapter: 'sqlite3', database: 'db/database.db' })
-          collection = datasource.get_collection('RelatedModel2')
+          collection = described_class.new(datasource, RelatedModel2)
           relation_schema = collection.schema[:fields]['custom_pk_model']
 
           # The foreign_key_target should be the custom primary_key specified in the association
@@ -114,12 +118,13 @@ module ForestAdminDatasourceActiveRecord
         it 'falls back to default primary key when no custom primary_key is specified' do
           stub_const('DefaultPkModel', Class.new(ApplicationRecord) do
             self.table_name = 'cars'
+            self.abstract_class = true
 
             has_one :user
           end)
 
           datasource = Datasource.new({ adapter: 'sqlite3', database: 'db/database.db' })
-          collection = datasource.get_collection('DefaultPkModel')
+          collection = described_class.new(datasource, DefaultPkModel)
           relation_schema = collection.schema[:fields]['user']
 
           # Should use the default primary key from the User model
@@ -132,6 +137,7 @@ module ForestAdminDatasourceActiveRecord
           stub_const('CompositePkModel1', Class.new(ApplicationRecord) do
             self.table_name = 'cars'
             self.primary_key = %w[reference category_id]
+            self.abstract_class = true
 
             has_one :related_composite, class_name: 'RelatedComposite1', foreign_key: %w[car_reference car_category],
                                         primary_key: %w[reference category_id]
@@ -139,10 +145,11 @@ module ForestAdminDatasourceActiveRecord
 
           stub_const('RelatedComposite1', Class.new(ApplicationRecord) do
             self.table_name = 'users'
+            self.abstract_class = true
           end)
 
           datasource = Datasource.new({ adapter: 'sqlite3', database: 'db/database.db' })
-          collection = datasource.get_collection('CompositePkModel1')
+          collection = described_class.new(datasource, CompositePkModel1)
           relation_schema = collection.schema[:fields]['related_composite']
 
           # With composite keys, origin_key_target is converted to a string representation
@@ -154,17 +161,19 @@ module ForestAdminDatasourceActiveRecord
           stub_const('CompositePkModel2', Class.new(ApplicationRecord) do
             self.table_name = 'cars'
             self.primary_key = %w[reference category_id]
+            self.abstract_class = true
           end)
 
           stub_const('RelatedComposite2', Class.new(ApplicationRecord) do
             self.table_name = 'users'
+            self.abstract_class = true
 
             belongs_to :composite_pk_model, class_name: 'CompositePkModel2', foreign_key: %w[car_reference car_category],
                                             primary_key: %w[reference category_id]
           end)
 
           datasource = Datasource.new({ adapter: 'sqlite3', database: 'db/database.db' })
-          collection = datasource.get_collection('RelatedComposite2')
+          collection = described_class.new(datasource, RelatedComposite2)
           relation_schema = collection.schema[:fields]['composite_pk_model']
 
           # With composite keys in belongs_to, foreign_key_target remains as an array
@@ -177,16 +186,19 @@ module ForestAdminDatasourceActiveRecord
         it 'returns string when custom primary_key is a symbol' do
           stub_const('SymbolPkModel', Class.new(ApplicationRecord) do
             self.table_name = 'cars'
+            self.abstract_class = true
 
             has_one :related, class_name: 'RelatedSymbol', foreign_key: 'car_id', primary_key: :reference
           end)
 
           stub_const('RelatedSymbol', Class.new(ApplicationRecord) do
             self.table_name = 'users'
+            self.abstract_class = true
           end)
 
           datasource = Datasource.new({ adapter: 'sqlite3', database: 'db/database.db' })
-          collection = datasource.get_collection('SymbolPkModel')
+          # Create collection directly since abstract_class models aren't auto-discovered
+          collection = described_class.new(datasource, SymbolPkModel)
           relation_schema = collection.schema[:fields]['related']
 
           expect(relation_schema.origin_key_target).to be_a(String)
@@ -197,12 +209,13 @@ module ForestAdminDatasourceActiveRecord
           # When primary_key option is explicitly nil, should use association's default
           stub_const('NilPkModel', Class.new(ApplicationRecord) do
             self.table_name = 'cars'
+            self.abstract_class = true
 
             has_one :user, primary_key: nil
           end)
 
           datasource = Datasource.new({ adapter: 'sqlite3', database: 'db/database.db' })
-          collection = datasource.get_collection('NilPkModel')
+          collection = described_class.new(datasource, NilPkModel)
           relation_schema = collection.schema[:fields]['user']
 
           # Should fall back to the default primary key
