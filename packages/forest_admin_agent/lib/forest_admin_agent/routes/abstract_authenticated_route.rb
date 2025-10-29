ae8d4d7acb983d0ed61c5dd27ac23b5ea75c21cd
Dustin Bolink <dustin.bolink@riipen.com>
2025-10-29 06:21:49 -0700

    fix: safely load id for many-to-one relationships (#195)
    Co-authored-by: bolinkd <4382804+bolinkd@users.noreply.github.com>

--- packages/forest_admin_agent/lib/forest_admin_agent/routes/abstract_authenticated_route.rb
+++ packages/forest_admin_agent/lib/forest_admin_agent/routes/abstract_authenticated_route.rb
@@ -16,7 +16,7 @@ module ForestAdminAgent
         args[:params][:data][:relationships]&.map do |field, value|
           schema = @collection.schema[:fields][field]
 
-          record[schema.foreign_key] = value['data']['id'] if schema.type == 'ManyToOne'
+          record[schema.foreign_key] = value.dig('data', 'id') if schema.type == 'ManyToOne'
         end
 
         record || {}
