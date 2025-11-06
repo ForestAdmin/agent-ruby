1f72b39bb22f9fadafa9c948cfe5595998a6fce4
Matt <matthv@gmail.com>
2025-11-06 12:01:43 +0100

    fix: add origin type field & value attributes to many to many

--- packages/forest_admin_datasource_active_record/lib/forest_admin_datasource_active_record/collection.rb
+++ packages/forest_admin_datasource_active_record/lib/forest_admin_datasource_active_record/collection.rb
@@ -80,21 +80,27 @@ module ForestAdminDatasourceActiveRecord
       association.polymorphic?
     end
 
+    # rubocop:disable Metrics/BlockNesting
     def fetch_associations
       associations(@model, support_polymorphic_relations: @support_polymorphic_relations).each do |association|
         case association.macro
         when :has_one
           if association_primary_key?(association)
             if association.through_reflection?
+              through_reflection = association.through_reflection
+              is_polymorphic = through_reflection.options[:as].present?
+
               add_field(
                 association.name.to_s,
                 ForestAdminDatasourceToolkit::Schema::Relations::ManyToManySchema.new(
                   foreign_collection: format_model_name(association.klass.name),
-                  origin_key: association.through_reflection.foreign_key,
-                  origin_key_target: association.through_reflection.join_foreign_key,
+                  origin_key: through_reflection.foreign_key,
+                  origin_key_target: through_reflection.join_foreign_key,
                   foreign_key: association.join_foreign_key,
                   foreign_key_target: association.association_primary_key,
-                  through_collection: format_model_name(association.through_reflection.klass.name)
+                  through_collection: format_model_name(through_reflection.klass.name),
+                  origin_type_field: is_polymorphic ? through_reflection.type : nil,
+                  origin_type_value: is_polymorphic ? @model.name : nil
                 )
               )
             elsif association.inverse_of&.polymorphic?
@@ -151,15 +157,20 @@ module ForestAdminDatasourceActiveRecord
         when :has_many
           if association_primary_key?(association)
             if association.through_reflection?
+              through_reflection = association.through_reflection
+              is_polymorphic = through_reflection.options[:as].present?
+
               add_field(
                 association.name.to_s,
                 ForestAdminDatasourceToolkit::Schema::Relations::ManyToManySchema.new(
                   foreign_collection: format_model_name(association.klass.name),
-                  origin_key: association.through_reflection.foreign_key,
-                  origin_key_target: association.through_reflection.join_foreign_key,
+                  origin_key: through_reflection.foreign_key,
+                  origin_key_target: through_reflection.join_foreign_key,
                   foreign_key: association.join_foreign_key,
                   foreign_key_target: association.association_primary_key,
-                  through_collection: format_model_name(association.through_reflection.klass.name)
+                  through_collection: format_model_name(through_reflection.klass.name),
+                  origin_type_field: is_polymorphic ? through_reflection.type : nil,
+                  origin_type_value: is_polymorphic ? @model.name : nil
                 )
               )
             elsif association.inverse_of&.polymorphic?
@@ -199,6 +210,7 @@ module ForestAdminDatasourceActiveRecord
         end
       end
     end
+    # rubocop:enable Metrics/BlockNesting
 
     def warn_missing_polymorphic_columns(association)
       missing_columns = []
