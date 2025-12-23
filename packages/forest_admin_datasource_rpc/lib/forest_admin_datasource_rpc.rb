ae9b3f806519831a6489b9b53fc241f168d6a6be
Arnaud Moncel <arnaud.moncel43@gmail.com>
2025-12-23 16:26:15 +0100

    chore: fix perf after introspection

--- packages/forest_admin_datasource_rpc/lib/forest_admin_datasource_rpc.rb
+++ packages/forest_admin_datasource_rpc/lib/forest_admin_datasource_rpc.rb
@@ -53,6 +53,7 @@ module ForestAdminDatasourceRpc
             'Fatal: Unable to build RPC datasource - no introspection schema was provided and schema fetch failed'
     end
 
+    options.delete(:introspection)
     ForestAdminDatasourceRpc::Datasource.new(options, schema, schema_polling)
   end
 
