311e57eb7d8d5fe12d0cc41e4770a8a356033e71
Matt <matthv@gmail.com>
2026-02-05 14:09:10 +0100

    fix: handle missing fields gracefully and prevent infinite recursion in schema generation

--- packages/forest_admin_agent/spec/lib/forest_admin_agent/utils/schema/generator_field_spec.rb
+++ packages/forest_admin_agent/spec/lib/forest_admin_agent/utils/schema/generator_field_spec.rb
@@ -111,6 +111,100 @@ module ForestAdminAgent
             ]
           )
         end
+
+        context 'when relation has missing fields' do
+          before do
+            @datasource_with_bad_relations = Datasource.new
+
+            collection_order = Collection.new(@datasource_with_bad_relations, 'Order')
+            collection_order.add_fields(
+              {
+                'id' => ColumnSchema.new(column_type: 'Number', is_primary_key: true),
+                # user_id field is missing but relation references it
+                'user' => Relations::ManyToOneSchema.new(
+                  foreign_key: 'missing_user_id',
+                  foreign_key_target: 'id',
+                  foreign_collection: 'User'
+                )
+              }
+            )
+
+            collection_user = Collection.new(@datasource_with_bad_relations, 'User')
+            collection_user.add_fields(
+              {
+                'id' => ColumnSchema.new(column_type: 'Number', is_primary_key: true),
+                'name' => ColumnSchema.new(column_type: 'String')
+              }
+            )
+
+            @datasource_with_bad_relations.add_collection(collection_order)
+            @datasource_with_bad_relations.add_collection(collection_user)
+          end
+
+          it 'returns nil and logs a warning when foreign_key field is missing in ManyToOne relation' do
+            logger = instance_spy(Logger)
+            allow(Facades::Container).to receive(:logger).and_return(logger)
+
+            schema = described_class.build_schema(
+              @datasource_with_bad_relations.get_collection('Order'),
+              'user'
+            )
+
+            expect(schema).to be_nil
+            expect(logger).to have_received(:log).with(
+              'Warn',
+              "Field 'missing_user_id' (foreign_key) not found in collection 'Order' for relation 'user'. " \
+              'This relation will be skipped. Check if the field exists in your database.'
+            )
+          end
+        end
+
+        context 'when OneToMany relation has missing origin_key' do
+          before do
+            @datasource_with_bad_one_to_many = Datasource.new
+
+            collection_author = Collection.new(@datasource_with_bad_one_to_many, 'Author')
+            collection_author.add_fields(
+              {
+                'id' => ColumnSchema.new(column_type: 'Number', is_primary_key: true),
+                'books' => Relations::OneToManySchema.new(
+                  origin_key: 'missing_author_id',
+                  origin_key_target: 'id',
+                  foreign_collection: 'Book'
+                )
+              }
+            )
+
+            collection_book = Collection.new(@datasource_with_bad_one_to_many, 'Book')
+            collection_book.add_fields(
+              {
+                'id' => ColumnSchema.new(column_type: 'Number', is_primary_key: true),
+                'title' => ColumnSchema.new(column_type: 'String')
+                # author_id is missing
+              }
+            )
+
+            @datasource_with_bad_one_to_many.add_collection(collection_author)
+            @datasource_with_bad_one_to_many.add_collection(collection_book)
+          end
+
+          it 'returns nil and logs a warning when origin_key field is missing' do
+            logger = instance_spy(Logger)
+            allow(Facades::Container).to receive(:logger).and_return(logger)
+
+            schema = described_class.build_schema(
+              @datasource_with_bad_one_to_many.get_collection('Author'),
+              'books'
+            )
+
+            expect(schema).to be_nil
+            expect(logger).to have_received(:log).with(
+              'Warn',
+              "Field 'missing_author_id' (origin_key) not found in collection 'Book' for relation 'books'. " \
+              'This relation will be skipped. Check if the field exists in your database.'
+            )
+          end
+        end
       end
     end
   end
