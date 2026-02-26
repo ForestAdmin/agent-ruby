4f9577b89ca6312ccc6d6e53403d3bafe0bc5183
Matthieu <matthv@gmail.com>
2026-02-26 16:03:15 +0100

    fix(active_record): use proper connection pool handling for rails 8 compatibility (#271)

--- packages/forest_admin_datasource_active_record/spec/lib/forest_admin_datasource_active_record/datasource_spec.rb
+++ packages/forest_admin_datasource_active_record/spec/lib/forest_admin_datasource_active_record/datasource_spec.rb
@@ -14,5 +14,59 @@ module ForestAdminDatasourceActiveRecord
       collections = datasource.collections.keys
       expect(collections).to all(be_a(String))
     end
+
+    describe '#execute_native_query' do
+      let(:datasource) do
+        ds = described_class.allocate
+        ds.instance_variable_set(:@live_query_connections, { 'main' => 'primary' })
+        ds
+      end
+
+      context 'when the connection does not exist' do
+        it 'raises a NotFoundError with the connection name' do
+          expect do
+            datasource.execute_native_query('unknown', 'SELECT 1', [])
+          end.to raise_error(
+            ForestAdminAgent::Http::Exceptions::NotFoundError,
+            /Native query connection 'unknown' is unknown/
+          )
+        end
+      end
+
+      context 'when the connection exists' do
+        it 'executes the query via pool.with_connection and returns results' do
+          mock_result = ActiveRecord::Result.new(['test'], [[1]])
+          mock_conn = instance_double(ActiveRecord::ConnectionAdapters::AbstractAdapter)
+          allow(mock_conn).to receive(:exec_query).and_return(mock_result)
+
+          mock_pool = instance_double(ActiveRecord::ConnectionAdapters::ConnectionPool)
+          allow(mock_pool).to receive(:with_connection).and_yield(mock_conn)
+
+          allow(ActiveRecord::Base).to receive(:connects_to).and_return([mock_pool])
+
+          result = datasource.execute_native_query('main', 'SELECT 1 as test', [])
+
+          expect(mock_pool).to have_received(:with_connection)
+          expect(mock_conn).to have_received(:exec_query).with('SELECT 1 as test', "SQL Native Query on 'main'", [])
+          expect(result).to eq([{ test: 1 }])
+        end
+      end
+
+      context 'when the query raises an error' do
+        it 'wraps the error in a ForestException' do
+          mock_pool = instance_double(ActiveRecord::ConnectionAdapters::ConnectionPool)
+          allow(mock_pool).to receive(:with_connection).and_raise(StandardError, 'syntax error')
+
+          allow(ActiveRecord::Base).to receive(:connects_to).and_return([mock_pool])
+
+          expect do
+            datasource.execute_native_query('main', 'INVALID SQL', [])
+          end.to raise_error(
+            ForestAdminDatasourceToolkit::Exceptions::ForestException,
+            /Error when executing SQL query.*syntax error/
+          )
+        end
+      end
+    end
   end
 end
