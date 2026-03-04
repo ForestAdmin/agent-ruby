d0b1d19d582f0b162e056154e91969c75d914ce8
Matt <matthv@gmail.com>
2026-03-04 15:00:00 +0100

    fix: use a local variable in the queue_customisation lambda

--- packages/forest_admin_datasource_customizer/lib/forest_admin_datasource_customizer/datasource_customizer.rb
+++ packages/forest_admin_datasource_customizer/lib/forest_admin_datasource_customizer/datasource_customizer.rb
@@ -27,22 +27,23 @@ module ForestAdminDatasourceCustomizer
     end
 
     def add_datasource(datasource, options)
+      original_datasource = datasource
       @stack.queue_customization(lambda {
+        ds = original_datasource
+
         if options[:include] || options[:exclude]
-          publication_decorator = Decorators::Publication::PublicationDatasourceDecorator.new(datasource)
+          publication_decorator = Decorators::Publication::PublicationDatasourceDecorator.new(ds)
           publication_decorator.keep_collections_matching(options[:include], options[:exclude])
-          datasource = publication_decorator
+          ds = publication_decorator
         end
 
         if options[:rename]
-          rename_collection_decorator = Decorators::RenameCollection::RenameCollectionDatasourceDecorator.new(
-            datasource
-          )
+          rename_collection_decorator = Decorators::RenameCollection::RenameCollectionDatasourceDecorator.new(ds)
           rename_collection_decorator.rename_collections(options[:rename])
-          datasource = rename_collection_decorator
+          ds = rename_collection_decorator
         end
 
-        @composite_datasource.add_data_source(datasource)
+        @composite_datasource.add_data_source(ds)
       })
 
       self
